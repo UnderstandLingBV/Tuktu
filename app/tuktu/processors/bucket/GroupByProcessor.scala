af8952ee52ea9839bbf524f67b0f84da2403c640
Erik Tromp <e.t.tromp@gmail.com>
2015-04-24 15:56:57 +0200

    GroupBy supports multiple fields (not fully tested yet)

--- app/tuktu/processors/bucket/GroupByProcessor.scala
+++ app/tuktu/processors/bucket/GroupByProcessor.scala
@@ -8,25 +8,32 @@ import tuktu.api.DataPacket
  * Groups data in the specified field.
  */
 class GroupByProcessor(resultName: String) extends BaseBucketProcessor(resultName) {
-    var field = ""
+    var fields: List[String] = _
+    var flatten = false
     
     override def initialize(config: JsObject) = {
         // Get the field to group on
-        field = (config \ "field").as[String]
+        fields = (config \ "fields").as[List[String]]
+        flatten = (config \ "flatten").asOpt[Boolean].getOrElse(false)
     }
     
     override def processor(): Enumeratee[DataPacket, DataPacket] = super.processor
     
-    override def doProcess(data: List[Map[String, Any]]): List[Map[String, Any]] = {     
-        val grouped = data.groupBy(_(field))
-        var groupedResult = collection.mutable.Map[String, Any]()
+    override def doProcess(data: List[Map[String, Any]]): List[Map[String, Any]] = {
+        var groupMap = collection.mutable.Map[String, Any]()
         
-        List((for (group <- grouped) yield {
-            (group._1 match {
-                case g: String => g
-                case _ => ""
-            }) -> group._2
-        }).toMap)        
+        // Build the map
+        List(groupBuilder(data, fields))
     }
-
+    
+    def groupBuilder(data: List[Map[String, Any]], fieldsLeft: List[String]): Map[String, Any] = fieldsLeft match {
+        case field::Nil => {
+            // Last field
+            data.groupBy(_(field).toString)
+        }
+        case field::remainder => {
+            // First group, then ascend
+            data.groupBy(_(field).toString).map(elem => elem._1 -> groupBuilder(elem._2, remainder))
+        }
+    }
 }
