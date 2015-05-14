cfe910e730290905425451e4cea71a3b338eb052
Mirek Vink <info@mirekvink.nl>
2015-05-14 14:15:24 +0200

    Updated HDFS writer

--- modules/nosql/app/tuktu/nosql/processors/hdfs.scala
+++ modules/nosql/app/tuktu/nosql/processors/hdfs.scala
@@ -1,41 +1,55 @@
 package tuktu.nosql.processors
 
-import tuktu.api._
-import play.api.libs.json.JsObject
-import play.api.libs.iteratee.Enumeratee
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+
+import play.api.libs.iteratee.Enumeratee
+import play.api.libs.json.JsObject
+import tuktu.api.BaseProcessor
+import tuktu.api.DataPacket
 
 /**
  * Writes specific fields of the datapacket out to HDFS, by default as JSON
  */
 class HDFSWriterProcessor(resultName: String) extends BaseProcessor(resultName) {
-    var hdfsUri = ""
-    var fileName = ""
+    var fileName: Path = _
     var fields: List[String] = _
-    var outputFormat = ""
+    var fieldSeparator = ""
+    var dataPacketSeparator = ""
+    var conf: Configuration = _
     
     override def initialize(config: JsObject) = {
-        hdfsUri = (config \ "uri").as[String]
-        fileName = (config \ "file_name").as[String]
+        val hdfsUri = (config \ "uri").as[String]
+        fileName = new Path((config \ "file_name").as[String])
         fields = (config \ "fields").as[List[String]]
-        outputFormat = (config \ "format").as[String]
+        fieldSeparator = (config \ "field_separator").as[String]
+        dataPacketSeparator = (config \ "datapacket_separator").as[String]
+        conf = new Configuration()
+        conf.set("fs.defaultFS", hdfsUri)
     }
     
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {
-        /*Configuration configuration = new Configuration();
-        FileSystem hdfs = FileSystem.get( new URI(hdfsUri), configuration );
-        Path file = new Path(hdfsUri + "/" + fileName);
-        if ( hdfs.exists( file )) { hdfs.delete( file, true ); } 
-        OutputStream os = hdfs.create( file,
-            new Progressable() {
-                public void progress() {
-                    out.println("...bytes written: [ "+bytesWritten+" ]");
-                } });
-        BufferedWriter br = new BufferedWriter( new OutputStreamWriter( os, "UTF-8" ) );
-        br.write("Hello World");
-        br.close();
-        hdfs.close();*/
+        val fs = FileSystem.get(conf)
+        val os = {            
+            if(fs.exists(fileName)) 
+                fs.append(fileName)
+            else
+                fs.create(fileName)
+        }
+        for (datum <- data.data) {
+            for (field <- fields) {
+                os.write((datum(field).toString + fieldSeparator).getBytes)
+            }
+            os.write(dataPacketSeparator.getBytes)    
+        }
+        os.close
+        fs.close
+        
         data
     })
+
 }
