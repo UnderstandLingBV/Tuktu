4e3dd44d0806d0625f0003f4e27cb9f977f5e00d
Mirek Vink <info@mirekvink.nl>
2015-05-15 13:37:18 +0200

    Updated HDFS writer

--- modules/nosql/app/tuktu/nosql/processors/hdfs.scala
+++ modules/nosql/app/tuktu/nosql/processors/hdfs.scala
@@ -2,22 +2,21 @@ package tuktu.nosql.processors
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
-
 import play.api.libs.iteratee.Enumeratee
 import play.api.libs.json.JsObject
 import tuktu.api.BaseProcessor
 import tuktu.api.DataPacket
+import org.apache.hadoop.fs.FSDataOutputStream
 
 /**
  * Writes specific fields of the datapacket out to HDFS, by default as JSON
  */
 class HDFSWriterProcessor(resultName: String) extends BaseProcessor(resultName) {
     // The filename to write to
-    var fileName: Path = _
+    var fileName: String = _
     // The fields used to write to HDFS
     var fields: List[String] = _
     // The separator between fields
@@ -29,7 +28,7 @@ class HDFSWriterProcessor(resultName: String) extends BaseProcessor(resultName) 
     
     override def initialize(config: JsObject) = {
         val hdfsUri = (config \ "uri").as[String]
-        fileName = new Path((config \ "file_name").as[String])
+        fileName = (config \ "file_name").as[String]
         fields = (config \ "fields").as[List[String]]
         fieldSeparator = (config \ "field_separator").as[String]
         dataPacketSeparator = (config \ "datapacket_separator").as[String]
@@ -42,19 +41,36 @@ class HDFSWriterProcessor(resultName: String) extends BaseProcessor(resultName) 
     
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {        
         val fs = FileSystem.get(conf)
-        val os = {            
-            if(fs.exists(fileName)) 
-                fs.append(fileName)
-            else
-                fs.create(fileName)
-        }
+
+        // Create a Map containing all the writers
+        val writers = scala.collection.mutable.Map[String, FSDataOutputStream]()
+        
         for (datum <- data.data) {
+            val name = tuktu.api.utils.evaluateTuktuString(fileName, datum)
+            
+            val os = {
+                if(writers.contains(name)) {
+                    writers(name)
+                }
+                else {
+                    val path = new Path(name)                    
+                    // Create or append
+                    writers += name -> {
+                        if (fs.exists(path)) fs.append(path)
+                        else fs.create(path)
+                    }                    
+                    writers(name)
+                }
+            }
+            
             for (field <- fields) {
                 os.write((datum(field).toString + fieldSeparator).getBytes)
             }
             os.write(dataPacketSeparator.getBytes)    
         }
-        os.close
+        // close all writers
+        writers.foreach(_._2.close)
+        //close filesystem
         fs.close
         
         data
