1b5c8bf92dc118d32f0b6fb862fa81876995c84d
NTPape <NTPape@users.noreply.github.com>
2015-06-11 16:14:09 +0200

    Rename file and change format handling of TimestampAdderProcessor.

--- app/tuktu/processors/time/TimestampAdderProcessor.scala
+++ app/tuktu/processors/time/TimestampAdderProcessor.scala
@@ -12,21 +12,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * Adds a simple timestamp to the data packet
  */
 class TimestampAdderProcessor(resultName: String) extends BaseProcessor(resultName) {
-    var format = ""
-    
+    var format: Option[String] = None
+
     override def initialize(config: JsObject) = {
-        format = (config \ "format").asOpt[String].getOrElse("")
+        format = (config \ "format").asOpt[String]
     }
-    
+
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => {
-        Future {new DataPacket(for (datum <- data.data) yield {
-            format match {
-                case "" => datum + (resultName -> System.currentTimeMillis)
-                case _ => {
-                    val dateFormat = new SimpleDateFormat(format)
-                    datum + (resultName -> dateFormat.format(System.currentTimeMillis))
+        Future {
+            new DataPacket(for (datum <- data.data) yield {
+                format match {
+                    case None => datum + (resultName -> System.currentTimeMillis)
+                    case Some(frmt) => {
+                        val dateFormat = new SimpleDateFormat(frmt)
+                        datum + (resultName -> dateFormat.format(System.currentTimeMillis))
+                    }
                 }
-            }
-        })}
+            })
+        }
     })
 }
