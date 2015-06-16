d213abf09972d3ccba1789d14bed22b64702e847
Erik Tromp <e.t.tromp@gmail.com>
2015-06-16 11:23:10 +0200

    Replaced HMM with more elaborate code from https://github.com/balshor/shimm

---
+++ modules/ml/app/tuktu/ml/models/hmm/Viterbi.scala
@@ -0,0 +1,43 @@
+package tuktu.ml.models.hmm
+
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.Map
+
+/**
+ * Implements Viterbi decoding for finding most likely traces
+ * 
+ * Code taken from https://github.com/balshor/shimm
+ * 
+ * @param observations The observed sequence
+ */
+class ViterbiAlgorithm(val observations: Seq[Int])(implicit val model: HiddenMarkovModel) {
+    import model._
+    
+    private val cache: Map[(Int, Int), (Double, Seq[Int])] = new HashMap
+
+    /** Computes (delta_t(i), psi_t(i)) */
+    def apply(t: Int, i: Int): (Double, Seq[Int]) = {
+
+        implicit val cmp = new Ordering[(Double, Seq[Int])] {
+            def compare(x: (Double, Seq[Int]), y: (Double, Seq[Int])): Int = {
+                java.lang.Double.compare(x._1, y._1)
+            }
+        }
+
+        if (cache contains (t, i)) {
+            cache((t, i))
+        } else {
+            val result: (Double, Seq[Int]) = if (t == 1) {
+                (Pi(i) * B(i, observations(t - 1)), Seq(i))
+            } else {
+                (0 to numberOfStates - 1) map { j =>
+                    val previousResult: (Double, Seq[Int]) = apply(t - 1, j)
+                    (previousResult._1 * A(j, i) * B(i, observations(t - 1)), previousResult._2 :+ j)
+                } max
+            }
+            cache((t, i)) = result
+            result
+        }
+    }
+
+}
