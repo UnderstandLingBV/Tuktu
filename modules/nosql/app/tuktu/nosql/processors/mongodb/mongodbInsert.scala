906e27ac7417daf94a1940f80a5fbc7b5222d905
Erik Tromp <e.t.tromp@gmail.com>
2015-07-17 10:00:11 +0200

    MongoDB now inserts in bulk

--- modules/nosql/app/tuktu/nosql/processors/mongodb/mongodbInsert.scala
+++ modules/nosql/app/tuktu/nosql/processors/mongodb/mongodbInsert.scala
@@ -1,21 +1,20 @@
 package tuktu.nosql.processors.mongodb
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+
+import play.api.Play.current
+import play.api.cache.Cache
 import play.api.libs.iteratee.Enumeratee
+import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.JsObject
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.api.MongoConnection
-import reactivemongo.api.MongoDriver
 import tuktu.api.BaseProcessor
 import tuktu.api.DataPacket
 import tuktu.api.utils.anyMapToJson
 import tuktu.nosql.util.MongoCollectionPool
 import tuktu.nosql.util.MongoSettings
-import play.api.cache.Cache
-import play.api.Play.current
-import scala.concurrent.duration._
-import scala.concurrent.Await
 
 /**
  * Inserts data into MongoDB
@@ -23,6 +22,7 @@ import scala.concurrent.Await
 class MongoDBInsertProcessor(resultName: String) extends BaseProcessor(resultName) { 
     var fields = List[String]()
     var collection: JSONCollection = _
+    var timeout: Int = _
 
     override def initialize(config: JsObject) {
         // Set up MongoDB client
@@ -36,16 +36,22 @@ class MongoDBInsertProcessor(resultName: String) extends BaseProcessor(resultNam
 
         // What fields to write?
         fields = (config \ "fields").as[List[String]]
+        
+        timeout = (config \ "timeout").asOpt[Int].getOrElse(Cache.getAs[Int]("timeout").getOrElse(5))
     }
 
-    override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.map((data: DataPacket) => {        
-        // Insert data into MongoDB
-        val futures = data.data.map(datum => fields match {
-            case Nil => collection.insert(anyMapToJson(datum, true))
-            case _ => collection.insert(anyMapToJson(datum.filter(elem => fields.contains(elem._1)), true))
+    override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.map((data: DataPacket) => {
+        // Convert to JSON
+        val bulkData = data.data.map(datum => fields match {
+            case Nil => anyMapToJson(datum, true)
+            case _ => anyMapToJson(datum.filter(elem => fields.contains(elem._1)), true)
         })
+        
+        // Bulk insert and await
+        val future = collection.bulkInsert(Enumerator.enumerate(bulkData))
+        
         // Wait for all the results to be retrieved
-        futures.foreach { f => if(!f.isCompleted) Await.ready(f, Cache.getAs[Int]("timeout").getOrElse(5) seconds) }
+        Await.ready(future, timeout seconds)
         
         data
     }) 
