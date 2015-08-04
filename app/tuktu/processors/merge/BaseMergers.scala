2dea8dbb9ac159ce33c9af6134b9d99fab60a006
Erik Tromp <e.t.tromp@gmail.com>
2015-08-04 09:28:27 +0200

    Added PaddingMerger

--- app/tuktu/processors/merge/BaseMergers.scala
+++ app/tuktu/processors/merge/BaseMergers.scala
@@ -11,8 +11,46 @@ class SimpleMerger() extends DataMerger() {
     override def merge(packets: List[DataPacket]): DataPacket = {
         new DataPacket(
                 // Fold and zip the packets into one
-                packets.map(packet => packet.data).fold(Nil)((x, y) => x.zipAll(y, Map(), Map()).map(x => x._1 ++ x._2))
+                packets.map(packet => packet.data).fold(Nil)((x, y) => x.zipAll(y, Map(), Map()).map(z => z._1 ++ z._2))
         )
+    }
+}
+
+/**
+ * Works the same as the SimpleMerger, but pads data (potentially repeatedly) if sizes are different
+ */
+class PaddingMerger() extends DataMerger() {
+    override def merge(packets: List[DataPacket]): DataPacket = {
+        new DataPacket({
+                /**
+                 * We must somehow deal with difference in length of the data packets
+                 * 
+                 * What we do here is check if one of the packets has size 1 and one of the others is lengthier. If so,
+                 * we pad the 1-sized packet to the length of the other. If both have a size bigger than one but
+                 * still not equal, we repeat the packet smallest in size
+                 */
+                val maxSize = packets.maxBy(pckt => pckt.data.size).data.size
+                
+                // Go over all packets, padding where required
+                packets.map(packet => packet.data).fold(Nil)((x, y) => {
+                    // Check size difference
+                    if (y.size < maxSize) {
+                        // Is size 1 or more?
+                        if (y.size == 1)
+                            // Repeat y's only value
+                            x.zipAll(y, Map(), y.head).map(z => z._1 ++ z._2)
+                        else {
+                            // Repeat the values of y
+                            x.zip({
+                                for (i <- 1 to maxSize) yield y(i % y.size)
+                            }).map(z => z._1 ++ z._2)
+                        }
+                    }
+                    else
+                        // Similar to simple merger
+                        x.zipAll(y, Map(), Map()).map(z => z._1 ++ z._2)
+                })
+        })
     }
 }
 
