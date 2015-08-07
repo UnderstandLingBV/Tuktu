56148c49273bff045e1a927bcfe345d8a4739ff3
Erik Tromp <e.t.tromp@gmail.com>
2015-08-07 16:37:36 +0200

    Updated ArithmeticProcessor

--- app/tuktu/processors/arithmetics/ArithmicsProcessor.scala
+++ app/tuktu/processors/arithmetics/ArithmicsProcessor.scala
@@ -1,21 +1,25 @@
 package tuktu.processors.arithmetics
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import play.api.libs.iteratee.Enumeratee
-import tuktu.api.DataPacket
 import play.api.libs.json.JsObject
 import tuktu.api.BaseProcessor
+import tuktu.api.DataPacket
 import tuktu.api.utils
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Calculates arithmetic
  */
 class ArithmeticProcessor(resultName: String) extends BaseProcessor(resultName) {
     var calculate: String = _
+    var numberOfDecimals: Int = 0
+    var doRounding = false
 
     override def initialize(config: JsObject) {
         calculate = (config \ "calculate").as[String]
+        numberOfDecimals = (config \ "numberOfDecimals").asOpt[Int].getOrElse(0)
+        doRounding = (config \ "doRounding").asOpt[Boolean].getOrElse(false)
     }
 
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => {
@@ -24,7 +28,10 @@ class ArithmeticProcessor(resultName: String) extends BaseProcessor(resultName) 
                 val formula = utils.evaluateTuktuString(calculate, datum)
                 val result = tuktu.utils.ArithmeticParser.readExpression(formula)
                 if (result.isDefined)
-                    datum + (resultName -> result.get())
+                    if(doRounding)
+                        datum + (resultName -> (math rint result.get() * math.pow(10, numberOfDecimals)) / math.pow(10, numberOfDecimals))
+                    else
+                        datum + (resultName -> result.get())
                 else
                     datum
             })
