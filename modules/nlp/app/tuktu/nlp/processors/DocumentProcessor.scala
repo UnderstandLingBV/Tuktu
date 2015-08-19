5c2f5ecf09c289ec8f0f7c339aa6207caff4de67
Erik Tromp <e.t.tromp@gmail.com>
2015-08-19 14:07:38 +0200

    Working out normalized summarization processor

---
+++ modules/nlp/app/tuktu/nlp/processors/DocumentProcessor.scala
@@ -0,0 +1,41 @@
+package tuktu.nlp.processors
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import play.api.libs.iteratee.Enumeratee
+import play.api.libs.json.JsObject
+import tuktu.api.BaseProcessor
+import tuktu.api.DataPacket
+
+/**
+ * Creates a document from a datapacket that contains multiple lines/sentences that need to be merged together
+ */
+class DocumentProcessor(resultName: String) extends BaseProcessor(resultName) {
+    var fieldName: String = _
+    var separator: String = _
+    var removeEmptyLines: Boolean = _
+
+    override def initialize(config: JsObject) {
+        // Get fields
+        fieldName = (config \ "field").as[String]
+        separator = (config \ "separator").asOpt[String].getOrElse(" ")
+        removeEmptyLines = (config \ "remove_empty_lines").asOpt[Boolean].getOrElse(true)
+    }
+    
+    override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
+        // Get all the text/line data from the packets
+        new DataPacket(List(Map(resultName -> (for {
+            datum <- data.data
+            
+            str = datum(fieldName) match {
+                case s: String => s
+                case s: Any => s.toString
+            }
+            
+            if (str.size > 0)
+        } yield {
+            str
+        }).mkString(separator))))
+    })
+}
--- modules/nlp/app/tuktu/nlp/processors/Summarize.scala
+++ modules/nlp/app/tuktu/nlp/processors/Summarize.scala
@@ -7,16 +7,21 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import nl.et4it.Tokenizer
 
+/**
+ * Summarizes text based on TF-IDF scores
+ */
 class SummarizeProcessor(resultName: String) extends BaseProcessor(resultName) {
     var textField: String = _
     var tfIdfField: String = _
     var numLines: Int = _
+    var optimalLength: Int = _
     var asPlainText: Boolean = _
     
     override def initialize(config: JsObject) {
         textField = (config \ "text_field").as[String]
         tfIdfField = (config \ "tfidf_field").as[String]
         numLines = (config \ "num_lines").as[Int]
+        optimalLength = Math.max(1, (config \ "optimal_sentence_length").asOpt[Int].getOrElse(11))
         asPlainText = (config \ "return_plain_text").asOpt[Boolean].getOrElse(true)
     }
     
@@ -38,9 +43,19 @@ class SummarizeProcessor(resultName: String) extends BaseProcessor(resultName) {
             val sortedLines = (for (line <- lines) yield {
                 val tokens = Tokenizer.tokenize(line)
                 (line, {
-                    tokens.foldLeft(0.0)((a, b) => a + {
+                    // Sum the TF-IDF scores
+                    val score = tokens.foldLeft(0.0)((a, b) => a + {
                         if (tfIdfScores.contains(b)) tfIdfScores(b) else 0.0
-                    }) / line.size.toDouble
+                    })
+                    // Normalize scores
+                    val normalizedScore = score / tokens.size.toDouble
+                    // Compensate for length
+                    val lenghtNormalizedScore = normalizedScore * Math.exp(-Math.abs(tokens.size - optimalLength))
+                    /*println("Score: "  + score + "\r\n" +
+                        "Normalized score: " + normalizedScore + "\r\n" +
+                        "Length normalized: " + lenghtNormalizedScore)*/
+                    
+                    lenghtNormalizedScore
                 })
             }).toList.sortBy(lineScore => lineScore._2).take(numLines).map(lineScore => lineScore._1)
             
