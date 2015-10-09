06c69159c68c6ac59706596f7c6704beaf1b7c73
ErikTromp <UnderstandLing>
2015-10-09 09:32:32 +0200

    More testing stuff

--- test/tuktu/test/flow/BaseFlowTester.scala
+++ test/tuktu/test/flow/BaseFlowTester.scala
@@ -1,11 +1,14 @@
 package tuktu.test.flow
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.ActorRef
+import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.actor.actorRef2Scala
+import akka.testkit.TestKit
 import controllers.Dispatcher
 import play.api.Play
 import play.api.Play.current
@@ -14,17 +17,29 @@ import play.api.libs.iteratee.Enumeratee
 import play.api.libs.json.JsObject
 import play.api.libs.json.Json
 import tuktu.api.DataPacket
+import tuktu.api.InitPacket
 import tuktu.api.StopPacket
-import scala.concurrent.Future
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration.DurationInt
 
+case class ResultPacket()
+
+/**
+ * Actor that will collect data packets
+ */
 class BaseFlowTesterCollector() extends Actor with ActorLogging {
     val buffer = collection.mutable.ListBuffer.empty[DataPacket]
     def receive() = {
         case dp: DataPacket => buffer += dp
         case sp: StopPacket => {}
+        case rp: ResultPacket => {}
     }
 }
 
+/**
+ * Class to forward data as if it were a processor
+ */
 class EnumForwarder(actor: ActorRef) {
     def apply(): Enumeratee[DataPacket, DataPacket] = Enumeratee.map((dp: DataPacket) => {
         actor ! dp
@@ -34,7 +49,12 @@ class EnumForwarder(actor: ActorRef) {
     })
 }
 
-class BaseFlowTester {
+/**
+ * Base flow tester class, should be invoked for testing flows
+ */
+class BaseFlowTester(timeoutSeconds: Int = 5) extends TestKit(ActorSystem("test")) {
+    implicit val timeout = Timeout(timeoutSeconds seconds)
+    
     def apply(outputs: List[List[DataPacket]], flowName: String): Future[Boolean] = {
         // Open the file and pass on
         val configFile = scala.io.Source.fromFile(Play.current.configuration.getString("tuktu.configrepo").getOrElse("configs") +
@@ -44,7 +64,7 @@ class BaseFlowTester {
         apply(outputs, cfg)
     }
     
-    def apply(outputs: List[List[DataPacket]], config: JsObject): Future[Boolean] = Future {
+    def apply(outputs: List[List[DataPacket]], config: JsObject): Future[Boolean] = {
         // Build processor map
         val processorMap = Dispatcher.buildProcessorMap((config \ "processors").as[List[JsObject]])
         
@@ -57,18 +77,37 @@ class BaseFlowTester {
         val next = (generator \ "next").as[List[String]]
         
         // Build the processor pipeline for this generator
-        val processorEnumeratees = for ((procEnum, index) <- Dispatcher.buildEnums(next, processorMap, None)._2.zipWithIndex) yield {
-            // Create actor that will fetch the results
-            val collectionActor = Akka.system.actorOf(Props(classOf[BaseFlowTesterCollector]),
-                    name = "testActor_" + java.util.UUID.randomUUID.toString)
-
-            // Append enumeratee with actor sending functionality
-            procEnum compose new EnumForwarder(collectionActor)()
+        val (enums, actors) = {
+            val enumActors = for ((procEnum, index) <- Dispatcher.buildEnums(next, processorMap, None)._2.zipWithIndex) yield {
+                // Create actor that will fetch the results
+                val collectionActor = Akka.system.actorOf(Props(classOf[BaseFlowTesterCollector]),
+                        name = "testActor_" + java.util.UUID.randomUUID.toString)
+    
+                // Append enumeratee with actor sending functionality
+                (
+                        procEnum compose new EnumForwarder(collectionActor)(),
+                        collectionActor
+                )
+            }
+            
+            (enumActors.map(_._1), enumActors.map(_._2)) 
         }
         
-        // Run the flow for each branch
+        // Set up the generator
+        val clazz = Class.forName(generatorName)
+        // Run the flow
+        val flow = Akka.system.actorOf(Props(clazz, resultName, enums, None),
+            name = clazz.getName +  "_" + java.util.UUID.randomUUID.toString
+        )
+        flow ! new InitPacket
+        flow ! generatorConfig
         
+        // Ask all the actors for completion
+        val results = Future.sequence(for (actor <- actors) yield (actor ? new ResultPacket()).asInstanceOf[Future[List[DataPacket]]])
         
-        true
+        // Inspect the results
+        results.map(obtainedOutput => {
+            true
+        })
     }
 }
