31ea26b4ea06287c7450fb6fa34c7b9eed7029f0
NTPape <NTPape@users.noreply.github.com>
2015-10-23 10:46:27 +0200

    Properly handle missing meta configs directory

--- modules/modeller/app/globals/ModellerGlobal.scala
+++ modules/modeller/app/globals/ModellerGlobal.scala
@@ -1,11 +1,12 @@
 package globals
 
 import java.io.File
-import java.nio.file.{Paths, Files}
+import java.nio.file.{ Path, Paths, Files }
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
+import scala.collection.JavaConversions.{ seqAsJavaList, asScalaBuffer }
 
 import com.fasterxml.jackson.core.JsonParseException
 
@@ -13,6 +14,7 @@ import akka.actor.Actor
 import akka.actor.Props
 import play.api.Application
 import play.api.GlobalSettings
+import play.api.Logger
 import play.api.Play
 import play.api.Play.current
 import play.api.cache.Cache
@@ -22,62 +24,72 @@ import play.api.libs.json.Json
 import tuktu.api.TuktuGlobal
 
 class ModellerGlobal() extends TuktuGlobal() {
-    def getDescriptors(files: List[File]) = {
+    def getDescriptors(files: List[Path]): Map[String, JsValue] = {
         (for {
-                file <- files
-                
-                (parseable, json) = try {
-                    // Open file and read contents into memory
-                    val content = scala.io.Source.fromFile(file)("utf-8")
-                    val js = Json.parse(content.getLines.mkString(""))
-                    content.close
-                    
-                    (true, js)
-                } catch {
-                    case e: JsonParseException => {
-                        System.err.println("Invalid JSON found in file " + file.getAbsoluteFile)
-                        (false, null)
-                    }
-                    case e: Exception => {
-                        e.printStackTrace
-                        (false, null)
-                    }
+            file <- files
+
+            (parseable, json) = try {
+                // Read file and try to parse as json
+                val js = Json.parse(Files.readAllBytes(file))
+
+                (true, js)
+            } catch {
+                case e: JsonParseException => {
+                    Logger.error("Invalid JSON found in meta config file: " + file.toAbsolutePath.normalize, e)
+                    (false, null)
                 }
-                
-                if (parseable)
+                case e: Throwable => {
+                    Logger.error("Other Exception while trying to read and parse meta config " + file.toAbsolutePath.normalize, e)
+                    (false, null)
+                }
+            }
+
+            if (parseable)
         } yield {
             // Get name and add
             (json \ "name").as[String] -> json
         }).toMap
     }
-    
-    def buildRecursiveMap(nameSoFar: String, dir: File): Map[String, Map[String, JsValue]] = {
+
+    def buildRecursiveMap(nameSoFar: List[Path], dir: Path): Map[String, Map[String, JsValue]] = {
         // Get name of this directory
-        val dirName = nameSoFar match {
-            case "" => dir.getName
-            case _ => nameSoFar + "." + dir.getName
-        }
+        val dirName = nameSoFar ++ List(dir.getFileName)
+
         // Rudimentary check to see if meta directory is available
-        if(!Files.exists(dir.toPath))
-          System.err.println("meta directory not available!")
-          
-        // Get all files and dirs separately
-        val (files, dirs) = dir.listFiles.toList.partition(!_.isDirectory)
-                
-        // Add all files, recurse over dirs
-        Map(dirName -> getDescriptors(files)) ++ dirs.map(dir => buildRecursiveMap(dirName, dir)).flatten
+        if (Files.isDirectory(dir)) {
+            // Define collector and partition files and directories
+            val collector = java.util.stream.Collectors.groupingBy[Path, Boolean](
+                new java.util.function.Function[Path, Boolean] {
+                    def apply(path: Path): Boolean = Files.isDirectory(path)
+                })
+            val map = Files.list(dir).collect(collector)
+
+            // Get configs
+            val configs = map.getOrDefault(false, Nil).toList
+            // Get subfolders
+            val subfolders = map.getOrDefault(true, Nil).toList
+
+            // Add all files, recurse over dirs
+            Map(dirName.mkString(".") -> getDescriptors(configs)) ++ subfolders.flatMap(dir => buildRecursiveMap(dirName, dir))
+        } else {
+            Map.empty
+        }
     }
-    
+
     def setCache() {
         // Cache the meta-repository location and load generator and processor descriptors
         val metaLocation = Play.current.configuration.getString("tuktu.metarepo").getOrElse("meta")
-        val genDir = new File(metaLocation + "/generators")
-        val procDir = new File(metaLocation + "/processors")
-        
+
+        if (!Files.isDirectory(Paths.get(metaLocation)))
+            Logger.error("Meta configs directory not available!")
+
+        val genDir = Paths.get(metaLocation, "generators")
+        val procDir = Paths.get(metaLocation, "processors")
+
         // Start with generators and processors root folder
-        val generators = buildRecursiveMap("", genDir).toList.sortBy(_._1).map(elem => (elem._1, elem._2.toList.sortBy(_._1)))
-        val processors = buildRecursiveMap("", procDir).toList.sortBy(_._1).map(elem => (elem._1, elem._2.toList.sortBy(_._1)))
-        
+        val generators = buildRecursiveMap(Nil, genDir).mapValues(_.toList.sortBy(_._1.toLowerCase)).toList.sortBy(_._1.toLowerCase)
+        val processors = buildRecursiveMap(Nil, procDir).mapValues(_.toList.sortBy(_._1.toLowerCase)).toList.sortBy(_._1.toLowerCase)
+
         // Cache
         Cache.set("generators", generators)
         Cache.set("processors", processors)
@@ -89,10 +101,10 @@ class ModellerGlobal() extends TuktuGlobal() {
     override def onStart(app: Application) {
         val metaActor = Akka.system.actorOf(Props(new Actor {
             def receive = {
-                case "cache" ⇒  setCache
+                case "cache" ⇒ setCache
             }
         }))
-        
+
         setCache
         // Load meta info, update every 5 minutes
         Akka.system.scheduler.schedule(
