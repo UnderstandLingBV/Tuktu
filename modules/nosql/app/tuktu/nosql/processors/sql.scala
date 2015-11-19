5a13d317a3d77c7504d795e60accfab469bcf766
NTPape <NTPape@users.noreply.github.com>
2015-11-19 10:10:29 +0100

    Added 'distinct' parameter to SQLProcessor

--- modules/nosql/app/tuktu/nosql/processors/sql.scala
+++ modules/nosql/app/tuktu/nosql/processors/sql.scala
@@ -15,11 +15,12 @@ class SQLProcessor(resultName: String) extends BaseProcessor(resultName) {
     var user = ""
     var password = ""
     var driver = ""
-    var append = false
     var query = ""
-    
+    var append = false
+    var distinct = false
+
     var client: client = _
-    
+
     override def initialize(config: JsObject) {
         // Get url, username and password for the connection; and the SQL driver (new drivers may have to be added to dependencies) and query
         url = (config \ "url").as[String]
@@ -27,43 +28,52 @@ class SQLProcessor(resultName: String) extends BaseProcessor(resultName) {
         password = (config \ "password").as[String]
         driver = (config \ "driver").as[String]
         query = (config \ "query").as[String]
-        
+
         // Append result or not?
         append = (config \ "append").asOpt[Boolean].getOrElse(false)
-        
-        // Set up the client
-        client = new client(url, user, password, driver)
+
+        // Only query distinct setups
+        distinct = (config \ "distinct").asOpt[Boolean].getOrElse(false)
     }
-    
+
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.map((data: DataPacket) => {
-        new DataPacket(for (datum <- data.data) yield {
+        val query_results = collection.mutable.Map.empty[(String, String, String, String, String), List[Map[String, Any]]]
+
+        val results = for (datum <- data.data) yield {
             // Evaluate all
-            val evalQuery = stringHandler.evaluateString(query, datum)            
+            val evalQuery = stringHandler.evaluateString(query, datum)
             val evalUrl = tuktu.api.utils.evaluateTuktuString(url, datum)
             val evalUser = tuktu.api.utils.evaluateTuktuString(user, datum)
             val evalPassword = tuktu.api.utils.evaluateTuktuString(password, datum)
             val evalDriver = tuktu.api.utils.evaluateTuktuString(driver, datum)
-            
-            // See if we need to update the client
-            val reEvaluated = if (evalUrl != url || evalUser != user || evalPassword != password || evalDriver != driver) {
-                client.close
-                client = new client(evalUrl, evalUser, evalPassword, evalDriver)
-                true
-            } else false
-            
-            // See if we need to append the result
-            append match {
-                case false => {
-                    // No need for appending
-                    client.query(evalQuery)
-                    datum
+
+            // If we only execute distinctly, skip if we already have a query result for these exact settings
+            if (distinct && query_results.contains((evalQuery, evalUrl, evalUser, evalPassword, evalDriver)))
+                query_results((evalQuery, evalUrl, evalUser, evalPassword, evalDriver))
+            else {
+                // See if we need to update the client
+                if (evalUrl != client.url || evalUser != client.user || evalPassword != client.password || evalDriver != client.driver) {
+                    client.close
+                    client = new client(evalUrl, evalUser, evalPassword, evalDriver)
                 }
-                case true => {
-                    // Get the result and use it
+                // See if we need to append or not
+                if (append) {
+                    client.query(evalQuery)
+                    query_results += (evalQuery, evalUrl, evalUser, evalPassword, evalDriver) -> Nil
+                    Nil
+                } else {
                     val res = client.queryResult(evalQuery).map(row => rowToMap(row))
-                    datum + (resultName -> res)
+                    query_results += (evalQuery, evalUrl, evalUser, evalPassword, evalDriver) -> res
+                    res
                 }
             }
-        })
+        }
+
+        // If we have to append data, zip datums with query results, otherwise return DataPacket untouched
+        if (append) {
+            new DataPacket(data.data.zip(results).map(tuple => tuple._1 + (resultName -> tuple._2)))
+        } else {
+            data
+        }
     }) compose Enumeratee.onEOF(() => client.close)
 }
