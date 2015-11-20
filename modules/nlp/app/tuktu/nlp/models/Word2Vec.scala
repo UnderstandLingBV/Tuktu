72b8bbe4aefa2b3818d0bfb2c55ae58558111f91
NTPape <NTPape@users.noreply.github.com>
2015-11-20 15:50:10 +0100

    Fixed problem with disjoint input and wordMap & formatting

--- modules/nlp/app/tuktu/nlp/models/Word2Vec.scala
+++ modules/nlp/app/tuktu/nlp/models/Word2Vec.scala
@@ -29,211 +29,215 @@ import org.nd4s.Implicits.floatArray2INDArray
 import tuktu.ml.models.BaseModel
 
 class Word2Vec() extends BaseModel {
-  // Keep track of word counts
-  var wordMap: HashMap[String, INDArray] = new HashMap[String, INDArray]()
-  // size of the input layers
-  var layerSize = 300
+    // Keep track of word counts
+    var wordMap: HashMap[String, INDArray] = new HashMap[String, INDArray]()
+    // size of the input layers
+    var layerSize = 300
 
-  /**
-   * Calculate top nearest words to the given list of words.
-   * 
-   * @param inputWords The word to find nearest words to
-   * @param top How many words should be returned
-   */
-  def wordsNearest(inputWords: Seq[String], top: Integer) = {
-    // get rid of words not available in wordMap
-    val clean = inputWords.filter(wordMap.contains(_))
-    // container for all the arrays
-    val words: INDArray = Nd4j.create(clean.size, layerSize)
-    // add words to the container
-    for ((word, row) <- clean.zipWithIndex)
-      words.putRow(row, wordMap(word))
-    // a queue with all the distances to the given words
-    val distances = new PriorityQueue[(String, Double)]()(Ordering.by[(String, Double), Double](_._2))
-    // calculate center of given words
-    val mean = {
-      if (words.isMatrix) words.mean(0)
-      else words
-    }
-    // calculate distances
-    val futures: Seq[Future[(String, Double)]] = (wordMap.map {
-      case (word, vector) => {
-        Future {
-          (word, Transforms.cosineSim(mean, vector))
+    /**
+     * Calculate top nearest words to the given list of words.
+     *
+     * @param inputWords The word to find nearest words to
+     * @param top How many words should be returned
+     */
+    def wordsNearest(inputWords: Seq[String], top: Integer): Map[String, Double] = {
+        // get rid of words not available in wordMap
+        val clean = inputWords.filter(wordMap.contains(_))
+        if (clean.isEmpty)
+            Map.empty
+        else {
+            // container for all the arrays
+            val words: INDArray = Nd4j.create(clean.size, layerSize)
+            // add words to the container
+            for ((word, row) <- clean.zipWithIndex)
+                words.putRow(row, wordMap(word))
+            // a queue with all the distances to the given words
+            val distances = new PriorityQueue[(String, Double)]()(Ordering.by[(String, Double), Double](_._2))
+            // calculate center of given words
+            val mean = {
+                if (words.isMatrix) words.mean(0)
+                else words
+            }
+            // calculate distances
+            val futures: Seq[Future[(String, Double)]] = (wordMap.map {
+                case (word, vector) => {
+                    Future {
+                        (word, Transforms.cosineSim(mean, vector))
+                    }
+                }
+            }).toSeq
+            // aggregate all the futures
+            val aggregated = Future.sequence(futures)
+            // wait a long time for it to be finished
+            val sims = Await.result(aggregated, 15.minutes)
+            // add all to the queue
+            sims.foreach(f => distances.enqueue(f._1 -> f._2))
+
+            // remove words which were in the input
+            var result = MutableList[(String, Double)]()
+            while (result.size < top && distances.nonEmpty) {
+                val w = distances.dequeue
+                if (!clean.contains(w._1))
+                    result += w
+            }
+            // return result
+            result.toMap
         }
-      }
-    }).toSeq
-    // aggregate all the futures
-    val aggregated = Future.sequence(futures)
-    // wait a long time for it to be finished
-    val sims = Await.result(aggregated, 15.minutes)
-    // add all to the queue
-    sims.foreach(f => distances.enqueue(f._1 -> f._2))
-
-    // remove words which were in the input
-    var result = MutableList[(String, Double)]()
-    while (result.size < top) {
-      val w = distances.dequeue
-      if (!clean.contains(w._1))
-        result += w
     }
-    // return result
-    result.toMap
-  }
 
-  override def serialize(filename: String): Unit = {
-    // Write out word counts
-    val oos = new ObjectOutputStream(new FileOutputStream(filename))
-    oos.writeObject(Map(
-      "wordMap" -> wordMap,
-      "layerSize" -> layerSize))
-    oos.close
-  }
+    override def serialize(filename: String): Unit = {
+        // Write out word counts
+        val oos = new ObjectOutputStream(new FileOutputStream(filename))
+        oos.writeObject(Map(
+            "wordMap" -> wordMap,
+            "layerSize" -> layerSize))
+        oos.close
+    }
 
-  override def deserialize(filename: String): Unit = {
-    // Load word counts
-    val ois = new ObjectInputStream(new FileInputStream(filename))
-    val obj = ois.readObject.asInstanceOf[Map[String, Any]]
-    ois.close
+    override def deserialize(filename: String): Unit = {
+        // Load word counts
+        val ois = new ObjectInputStream(new FileInputStream(filename))
+        val obj = ois.readObject.asInstanceOf[Map[String, Any]]
+        ois.close
 
-    // Set back weights
-    wordMap = obj("wordMap").asInstanceOf[HashMap[String, INDArray]]
-    layerSize = obj("layerSize").asInstanceOf[Int]
-  }
+        // Set back weights
+        wordMap = obj("wordMap").asInstanceOf[HashMap[String, INDArray]]
+        layerSize = obj("layerSize").asInstanceOf[Int]
+    }
 
-  /**
-   * Load a word2vec file in the following format:
-   * word [vector]
-   * @param file Location of the file
-   */
-  def loadTextModel(file: String) = {
-    wordMap.clear
-    try {
-      val modelFile = new File(file)
-      val br = new BufferedReader(new InputStreamReader(
-        {
-          if (GzipUtils.isCompressedFilename(modelFile.getName()))
-            new GZIPInputStream(new FileInputStream(modelFile))
-          else
-            new FileInputStream(modelFile)
-        }))
+    /**
+     * Load a word2vec file in the following format:
+     * word [vector]
+     * @param file Location of the file
+     */
+    def loadTextModel(file: String) = {
+        wordMap.clear
+        try {
+            val modelFile = new File(file)
+            val br = new BufferedReader(new InputStreamReader(
+                {
+                    if (GzipUtils.isCompressedFilename(modelFile.getName()))
+                        new GZIPInputStream(new FileInputStream(modelFile))
+                    else
+                        new FileInputStream(modelFile)
+                }))
 
-      Stream.continually(br.readLine()).takeWhile(_ != null).foreach { line =>
-        {
-          val split = line.split(" ").toList
-          val word = split(0)
-          layerSize = split.length - 1
-          val vector = split.drop(1).map(java.lang.Float.parseFloat).toArray
+            Stream.continually(br.readLine()).takeWhile(_ != null).foreach { line =>
+                {
+                    val split = line.split(" ").toList
+                    val word = split(0)
+                    layerSize = split.length - 1
+                    val vector = split.drop(1).map(java.lang.Float.parseFloat).toArray
 
-          wordMap += word -> vector.toNDArray
+                    wordMap += word -> vector.toNDArray
+                }
+            }
+
+            // clean up
+            br.close
+        } catch {
+            case e: Exception => e.printStackTrace
         }
-      }
-
-      // clean up
-      br.close
-    } catch {
-      case e: Exception => e.printStackTrace
-    }
-  }
-
-  /**
-   * Load a binary Word2Vec file.
-   * @param file Location of the file.
-   */
-  def loadBinaryModel(file: String) {
-    wordMap.clear
-    try {
-      val modelFile = new File(file)
-      val bis = new BufferedInputStream(
-        {
-          if (GzipUtils.isCompressedFilename(modelFile.getName()))
-            new GZIPInputStream(new FileInputStream(modelFile))
-          else
-            new FileInputStream(modelFile)
-        })
-
-      val dis = new DataInputStream(bis)
-      val words = Integer.parseInt(readString(dis))
-      layerSize = Integer.parseInt(readString(dis))
-      for (i <- 0 to words - 1) {
-        val word = readString(dis)
-        if (i % 100000 == 0)
-          println("Loading " + word + " with word " + i)
-
-        val vector = (for (j <- 0 to layerSize - 1) yield {
-          readFloat(dis)
-        }).toArray
-
-        wordMap += word -> vector.toNDArray
-      }
-
-      // clean up
-      dis.close
-      bis.close
-
-    } catch {
-      case e: Exception => e.printStackTrace
     }
 
-  }
+    /**
+     * Load a binary Word2Vec file.
+     * @param file Location of the file.
+     */
+    def loadBinaryModel(file: String) {
+        wordMap.clear
+        try {
+            val modelFile = new File(file)
+            val bis = new BufferedInputStream(
+                {
+                    if (GzipUtils.isCompressedFilename(modelFile.getName()))
+                        new GZIPInputStream(new FileInputStream(modelFile))
+                    else
+                        new FileInputStream(modelFile)
+                })
 
-  val MAX_SIZE = 50
+            val dis = new DataInputStream(bis)
+            val words = Integer.parseInt(readString(dis))
+            layerSize = Integer.parseInt(readString(dis))
+            for (i <- 0 to words - 1) {
+                val word = readString(dis)
+                if (i % 100000 == 0)
+                    println("Loading " + word + " with word " + i)
 
-  /**
-   * Read a float from a data input stream Credit to:
-   * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
-   *
-   * @param is
-   * @return
-   * @throws IOException
-   */
-  def readFloat(is: InputStream) = {
-    var bytes: Array[Byte] = new Array[Byte](4)
-    is.read(bytes)
-    getFloat(bytes)
-  }
+                val vector = (for (j <- 0 to layerSize - 1) yield {
+                    readFloat(dis)
+                }).toArray
 
-  /**
-   * Read a string from a data input stream Credit to:
-   * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
-   *
-   * @param b
-   * @return
-   * @throws IOException
-   */
-  def getFloat(b: Array[Byte]) = {
-    var accum: Integer = 0
-    accum = accum | (b(0) & 0xff) << 0
-    accum = accum | (b(1) & 0xff) << 8
-    accum = accum | (b(2) & 0xff) << 16
-    accum = accum | (b(3) & 0xff) << 24
-    java.lang.Float.intBitsToFloat(accum)
-  }
+                wordMap += word -> vector.toNDArray
+            }
 
-  /**
-   * Read a string from a data input stream Credit to:
-   * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
-   *
-   * @param dis
-   * @return
-   * @throws IOException
-   */
-  def readString(dis: DataInputStream) = {
-    var bytes: Array[Byte] = new Array[Byte](MAX_SIZE)
-    var b = dis.readByte
-    var i = -1;
-    val sb = new StringBuilder
-    while (b != 32 && b != 10) {
-      i += 1
-      bytes(i) = b
-      b = dis.readByte
-      if (i == 49) {
-        sb.append(new String(bytes))
-        i = -1;
-        bytes = new Array[Byte](MAX_SIZE)
-      }
+            // clean up
+            dis.close
+            bis.close
+
+        } catch {
+            case e: Exception => e.printStackTrace
+        }
+
     }
-    sb.append(new String(bytes, 0, i + 1));
-    sb.toString
-  }
+
+    val MAX_SIZE = 50
+
+    /**
+     * Read a float from a data input stream Credit to:
+     * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
+     *
+     * @param is
+     * @return
+     * @throws IOException
+     */
+    def readFloat(is: InputStream) = {
+        var bytes: Array[Byte] = new Array[Byte](4)
+        is.read(bytes)
+        getFloat(bytes)
+    }
+
+    /**
+     * Read a string from a data input stream Credit to:
+     * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
+     *
+     * @param b
+     * @return
+     * @throws IOException
+     */
+    def getFloat(b: Array[Byte]) = {
+        var accum: Integer = 0
+        accum = accum | (b(0) & 0xff) << 0
+        accum = accum | (b(1) & 0xff) << 8
+        accum = accum | (b(2) & 0xff) << 16
+        accum = accum | (b(3) & 0xff) << 24
+        java.lang.Float.intBitsToFloat(accum)
+    }
+
+    /**
+     * Read a string from a data input stream Credit to:
+     * https://github.com/NLPchina/Word2VEC_java/blob/master/src/com/ansj/vec/Word2VEC.java
+     *
+     * @param dis
+     * @return
+     * @throws IOException
+     */
+    def readString(dis: DataInputStream) = {
+        var bytes: Array[Byte] = new Array[Byte](MAX_SIZE)
+        var b = dis.readByte
+        var i = -1;
+        val sb = new StringBuilder
+        while (b != 32 && b != 10) {
+            i += 1
+            bytes(i) = b
+            b = dis.readByte
+            if (i == 49) {
+                sb.append(new String(bytes))
+                i = -1;
+                bytes = new Array[Byte](MAX_SIZE)
+            }
+        }
+        sb.append(new String(bytes, 0, i + 1));
+        sb.toString
+    }
 }
--- modules/nlp/app/tuktu/nlp/processors/Word2Vec.scala
+++ modules/nlp/app/tuktu/nlp/processors/Word2Vec.scala
@@ -10,18 +10,18 @@ import tuktu.ml.processors.BaseMLApplyProcessor
  */
 class ReadGoogleWord2VecProcessor(resultName: String) extends BaseMLDeserializeProcessor[Word2Vec](resultName) {
     var binary: Boolean = _
-  
+
     override def initialize(config: JsObject) {
         binary = (config \ "binary").asOpt[Boolean].getOrElse(false)
         super.initialize(config)
     }
-  
+
     override def deserializeModel(filename: String) = {
         val model = new Word2Vec()
-        if (binary) 
-          model.loadBinaryModel(filename)
-        else 
-          model.loadTextModel(filename)        
+        if (binary)
+            model.loadBinaryModel(filename)
+        else
+            model.loadTextModel(filename)
         model
     }
 }
@@ -32,22 +32,23 @@ class ReadGoogleWord2VecProcessor(resultName: String) extends BaseMLDeserializeP
 class Word2VecNearestWordsProcessor(resultName: String) extends BaseMLApplyProcessor[Word2Vec](resultName) {
     var field: String = _
     var top: Int = _
-    
+
     override def initialize(config: JsObject) {
         field = (config \ "data_field").as[String]
         top = (config \ "top").asOpt[Int].getOrElse(10)
         super.initialize(config)
     }
-    
+
     override def applyModel(resultName: String, data: List[Map[String, Any]], model: Word2Vec): List[Map[String, Any]] = {
-        for (datum <- data) yield
+        for (datum <- data) yield {
             datum + (resultName -> {
                 // Check field type
                 datum(field) match {
                     case dtm: Seq[String] => model.wordsNearest(dtm, top)
-                    case dtm: String => model.wordsNearest(List(dtm), top)
-                    case dtm => model.wordsNearest(List(dtm.toString), top)
+                    case dtm: String      => model.wordsNearest(List(dtm), top)
+                    case dtm              => model.wordsNearest(List(dtm.toString), top)
                 }
             })
+        }
     }
 }
