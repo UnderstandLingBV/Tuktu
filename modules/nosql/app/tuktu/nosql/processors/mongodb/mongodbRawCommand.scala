b3c3deaccdbdb0a5927a2d07a4404534c0ea7511
David Massart <dmassart@acm.org>
2015-12-02 09:17:30 +0100

    Making MongoDB Raw Command non-blocking.

--- modules/nosql/app/tuktu/nosql/processors/mongodb/mongodbRawCommand.scala
+++ modules/nosql/app/tuktu/nosql/processors/mongodb/mongodbRawCommand.scala
@@ -42,7 +42,7 @@ class MongoDBRawCommandProcessor(resultName: String) extends BaseProcessor(resul
     var command: JsObject = _
     var db: DefaultDB = _
     var resultOnly: Boolean = _
-    var timeout: Int = _
+    // var timeout: Int = _
 
     override def initialize(config: JsObject) 
     {
@@ -60,9 +60,9 @@ class MongoDBRawCommandProcessor(resultName: String) extends BaseProcessor(resul
         resultOnly = (config \ "resultOnly").asOpt[Boolean].getOrElse(false)
         
         // Get the timeout of the service
-        timeout = (config \ "timeout").asOpt[Int].getOrElse(Cache.getAs[Int]("timeout").getOrElse(30))
+        // timeout = (config \ "timeout").asOpt[Int].getOrElse(Cache.getAs[Int]("timeout").getOrElse(30))
     }
-
+/*
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {
         new DataPacket(for (datum <- data.data) yield {
             
@@ -79,5 +79,22 @@ class MongoDBRawCommandProcessor(resultName: String) extends BaseProcessor(resul
             }
         })
     })
-     
+*/
+    
+  override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => {
+            val runner = Command.run( JSONSerializationPack )
+            val futureResult = runner.apply( db, runner.rawCommand( command ) ).one[JsObject]
+            futureResult.map{ result => 
+                if (resultOnly)
+                {
+                    new DataPacket(for (datum <- data.data) yield { datum + (resultName -> (result \ "result") ) })
+                }
+                else
+                {
+                    new DataPacket(for (datum <- data.data) yield { datum + (resultName -> result) })
+                }
+            }
+    })
+    
+    
 }
