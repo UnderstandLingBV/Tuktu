b41eff16e891627c3ed5e034bc9c345264c03f4a
NTPape <NTPape@users.noreply.github.com>
2015-12-18 10:14:44 +0100

    Use StringBuilder buffer instead of regular expressions

--- modules/nosql/app/tuktu/nosql/util/stringHandler.scala
+++ modules/nosql/app/tuktu/nosql/util/stringHandler.scala
@@ -1,54 +1,67 @@
 package tuktu.nosql.util
 
-import java.util.regex.Pattern
-
 object stringHandler {
-    val pattern = Pattern.compile("\\$\\{(.*?)\\}")
-    
     /**
      * Evaluates a string taking into account IN-statements
      */
-    def evaluateString(str: String, vars: Map[String, Any], quote: String ="'", escape: String = "'") = {
-        // Set up matcher and string buffer
-        val matcher = pattern.matcher(str)
-        val buff = new StringBuffer(str.length)
-        
-        // Replace with vars
-        while (matcher.find) {
-            // See if this is a list or not
-            val key = matcher.group(1).split(";")
-            
-            if (key.size > 0) {
-                // See if it's a sequence or single field
-                vars(key(0)) match {
-                    case seq: Seq[Any] if vars(key(0)).asInstanceOf[Seq[Any]].size > 0 => {
-                        // Iterate over the values
-                        val variableList = (for (singleVar <- vars(key(0)).asInstanceOf[Seq[Any]]) yield {
-                            // Parse depending on type
-                            singleVar match {
-                                case el: Int => el.toString
-                                case el: Double => el.toString
-                                case el: Long => el.toString
-                                case el: Boolean => if (el) "1" else "0"
-                                case el: Any => {
-                                    // Here we assume it's string
-                                    quote + el.toString.replaceAll(quote, escape+quote) + quote
-                                }
-                            }
-                        }).mkString(key(1))
-                        
-                        // Now append it to our string
-                        matcher.appendReplacement(buff, variableList)
-                    }
-                    case elem: String => matcher.appendReplacement(buff, quote + elem.replaceAll(quote, escape+quote) + quote)
-                    case _ => matcher.appendReplacement(buff, vars(key(0)).toString)
+    def evaluateString(str: String, vars: Map[String, Any], quote: String = "'", escape: String = "'") = {
+        // The result builder
+        val result = new StringBuilder
+        // A temporary buffer to determine if we need to replace this
+        val buffer = new StringBuilder
+        // The prefix length of TuktuStrings "${".length == 2
+        val prefixSize = "${".length
+
+        for (currentChar <- str) {
+            if (buffer.length == 0) {
+                if (currentChar.equals('$'))
+                    buffer.append(currentChar)
+                else
+                    result.append(currentChar)
+            } else if (buffer.length == 1) {
+                buffer.append(currentChar)
+                if (!currentChar.equals('{')) {
+                    result.append(buffer)
+                    buffer.clear
                 }
-            } else // Just regular string
-                matcher.appendReplacement(buff, vars(matcher.group(1)).toString)
+            } else {
+                if (currentChar.equals('}')) {
+                    // Extract key and additional parameters and clear buffer
+                    val key = buffer.substring(prefixSize).split(';')
+                    buffer.clear
+
+                    // See if it's a sequence or single field
+                    vars(key(0)) match {
+                        case seq: Seq[Any] if vars(key(0)).asInstanceOf[Seq[Any]].size > 0 => {
+                            // Iterate over the values
+                            val variableList = (for (singleVar <- vars(key(0)).asInstanceOf[Seq[Any]]) yield {
+                                // Parse depending on type
+                                singleVar match {
+                                    case el: Int     => el.toString
+                                    case el: Double  => el.toString
+                                    case el: Long    => el.toString
+                                    case el: Boolean => if (el) "1" else "0"
+                                    case el: Any => {
+                                        // Here we assume it's a String
+                                        quote + el.toString.replace(quote, escape + quote) + quote
+                                    }
+                                }
+                            }).mkString(key(1))
+
+                            // Now append it to our string
+                            result.append(variableList)
+                        }
+                        case elem: String => result.append(quote + elem.replace(quote, escape + quote) + quote)
+                        case _            => result.append(vars(key(0)).toString)
+                    }
+                } else {
+                    // Not '}' yet, keep appending the buffer
+                    buffer.append(currentChar)
+                }
+            }
         }
-        matcher.appendTail(buff)
-        
-        // Return buffer
-        buff.toString
+        // Add any left overs
+        result.append(buffer)
+        result.toString
     }
 }
