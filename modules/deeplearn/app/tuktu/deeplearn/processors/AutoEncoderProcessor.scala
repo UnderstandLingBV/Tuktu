e187b0419aa73644d904f9d45236b94f645e3b1d
ErikTromp <e.t.tromp@gmail.com>
2016-02-04 18:34:02 +0100

    Removed auto encoder

--- modules/deeplearn/app/tuktu/deeplearn/processors/AutoEncoderProcessor.scala
+++
@@ -1,7 +0,0 @@
-package tuktu.deeplearn.processors
-
-import tuktu.ml.processors.BaseMLTrainProcessor
-import tuktu.deeplearn.models.AutoEncoder
-
-class AutoEncoderTrainProcessor(resultName: String) extends BaseMLTrainProcessor[AutoEncoder](resultName) {
-}
