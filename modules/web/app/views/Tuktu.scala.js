2830c08e753936468d612b721b75aa6afc9cb7dd
ErikTromp <e.t.tromp@gmail.com>
2016-02-07 17:15:13 +0100

    Adding jquery if not there for Tuktu.js

--- modules/web/app/views/Tuktu.scala.js
+++ modules/web/app/views/Tuktu.scala.js
@@ -3,6 +3,13 @@ var tuktuvars;
 if (typeof tuktuvars === 'undefined') {
 	tuktuvars = {};
 }
+if (typeof jQuery === 'undefined') {
+	var h = document.getElementsByTagName("head")[0];
+    var s = s = document.createElement("script");
+	 s.setAttribute("type", "text/javascript");
+	 s.setAttribute("src", "//code.jquery.com/jquery-2.2.0.min.js");
+	 h.appendChild(s);
+}
 function tuktuFlow(nf) {
 	var h = document.getElementsByTagName("head")[0];
     var s = document.createElement("script");
