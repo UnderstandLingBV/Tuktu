11699473572bc9483661c66ed596b34aac7c24f2
David Massart <dmassart@acm.org>
2016-02-21 17:12:09 -0800

    Adds a datum to field processor.

---
+++ app/tuktu/processors/Datum2Field.scala
@@ -0,0 +1,19 @@
+package tuktu.processors
+
+import play.api.libs.iteratee.Enumeratee
+import play.api.libs.json.JsObject
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import tuktu.api._
+
+/**
+ * Moves a datum to a field
+ */
+class Datum2FieldProcessor(resultName: String) extends BaseProcessor(resultName) {
+
+    override def initialize(config: JsObject) {}
+
+    override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
+        new DataPacket( data.data.map( datum => Map(resultName -> datum)) ) 
+    })
+}
