15754cd2010188f6201f5a68dc1e498227d03dd8
NTPape <NTPape@users.noreply.github.com>
2016-07-05 09:15:00 +0200

    Fixed FunctionFetcherProcessor to actually execute the function

--- modules/web/app/tuktu/web/processors/analytics/FunctionProcessors.scala
+++ modules/web/app/tuktu/web/processors/analytics/FunctionProcessors.scala
@@ -18,13 +18,13 @@ class FunctionProcessor(resultName: String) extends BaseJsProcessor(resultName) 
     var name: String = _
     var params: List[String] = _
     var body: String = _
-    
+
     override def initialize(config: JsObject) {
         name = (config \ "name").as[String]
         params = (config \ "params").asOpt[List[String]].getOrElse(List[String]())
         body = (config \ "body").as[String]
     }
-    
+
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {
         for (datum <- data) yield {
             addJsElement(datum, new WebJsFunctionObject(
@@ -41,14 +41,14 @@ class FunctionProcessor(resultName: String) extends BaseJsProcessor(resultName) 
  */
 class FunctionFetcherProcessor(resultName: String) extends BaseJsProcessor(resultName) {
     var body: String = _
-    
+
     override def initialize(config: JsObject) {
         body = (config \ "body").as[String]
     }
-    
+
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {
         for (datum <- data) yield addJsElement(datum, new WebJsObject(
-                "function() {" + utils.evaluateTuktuString(body, datum) + "}", true
+            "(function(){" + utils.evaluateTuktuString(body, datum) + "})();", true
         ))
     })
 }
