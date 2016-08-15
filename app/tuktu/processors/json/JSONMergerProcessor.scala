2860414b5a489f91b8c268620d860a37d795df4e
Erik Tromp <e.t.tromp@gmail.com>
2016-08-15 13:50:31 +0200

    Using play-json 2.5's neater deep merge

--- app/tuktu/processors/json/JSONMergerProcessor.scala
+++ app/tuktu/processors/json/JSONMergerProcessor.scala
@@ -13,58 +13,39 @@ import play.api.libs.json.Json
  */
 class JSONMergerProcessor(resultName: String) extends BaseProcessor(resultName) {
     var fields: List[String] = _
-    
+
     override def initialize(config: JsObject) {
         fields = (config \ "fields").as[List[String]]
     }
-    
+
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
         new DataPacket(for (datum <- data.data) yield {
             // Get the data fields
             val objects = fields.map(field => datum(field).asInstanceOf[JsObject])
             // Merge them one by one
-            val json = objects.foldLeft(Json.obj())((a,b) => mergeJson(a, b))
-            
+            val json = objects.foldLeft(Json.obj())((a, b) => mergeJson(b, a))
+
             datum + (resultName -> json)
         })
     })
-    
+
     /**
      * Recursive function to merge two JSON objects
      */
     def mergeJson(a: JsObject, b: JsObject): JsObject = {
-        // Get all the keys and recurse
-        val aSet = a.fieldSet
-        val bSet = b.fieldSet
-        Json.toJson((for ((aKey, aVal) <- aSet) yield {
-            aKey -> {
-                if (b.keys.contains(aKey)) {
-                    // Get b's element
-                    val bVal = b.fieldSet.find(el => el._1 == aKey).getOrElse((aKey, JsNull))._2
-                    
-                    // We need to merge
-                    aVal match {
-                        case v: JsBoolean => v
-                        case v: JsValue if v == null => {
-                            // Check if we can use b's value
-                            bVal match {
-                                case JsNull => JsNull
-                                case w: JsValue => w
-                            }
-                        }
-                        case v: JsNumber => v
-                        case v: JsString => v
-                        case v: JsObject => {
-                            // Recurse
-                            bVal match {
-                                case w: JsObject => mergeJson(v, w)
-                                case w: JsValue => v
-                            }
-                        }
-                        case v: JsArray => v
+        def merge(existingObject: JsObject, otherObject: JsObject): JsObject = {
+            val result = existingObject.value ++ otherObject.value.map {
+                case (otherKey, otherValue) =>
+                    val maybeExistingValue = existingObject.value.get(otherKey)
+
+                    val newValue = (maybeExistingValue, otherValue) match {
+                        case (Some(e: JsObject), o: JsObject) => merge(e, o)
+                        case _                                => otherValue
                     }
-                } else aVal
+                    otherKey -> newValue
             }
-        }).toMap ++ bSet.filter(el => !a.keys.contains(el._1))).as[JsObject]
+            JsObject(result.toSeq)
+        }
+        merge(a, b)
     }
 }
