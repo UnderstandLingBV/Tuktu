9b2b4e5767e504c102141e65de0bad890177d37a
Erik Tromp <e.t.tromp@gmail.com>
2016-08-15 13:42:18 +0200

    Apparently Play-JSON has a deep merge itself

--- app/tuktu/processors/json/JSONMergerProcessor.scala
+++ app/tuktu/processors/json/JSONMergerProcessor.scala
@@ -23,48 +23,9 @@ class JSONMergerProcessor(resultName: String) extends BaseProcessor(resultName) 
             // Get the data fields
             val objects = fields.map(field => datum(field).asInstanceOf[JsObject])
             // Merge them one by one
-            val json = objects.foldLeft(Json.obj())((a,b) => mergeJson(a, b))
+            val json = objects.foldLeft(Json.obj())((a,b) => a.deepMerge(b))
             
             datum + (resultName -> json)
         })
     })
-    
-    /**
-     * Recursive function to merge two JSON objects
-     */
-    def mergeJson(a: JsObject, b: JsObject): JsObject = {
-        // Get all the keys and recurse
-        val aSet = a.fieldSet
-        val bSet = b.fieldSet
-        Json.toJson((for ((aKey, aVal) <- aSet) yield {
-            aKey -> {
-                if (b.keys.contains(aKey)) {
-                    // Get b's element
-                    val bVal = b.fieldSet.find(el => el._1 == aKey).getOrElse((aKey, JsNull))._2
-                    
-                    // We need to merge
-                    aVal match {
-                        case v: JsBoolean => v
-                        case v: JsValue if v == null => {
-                            // Check if we can use b's value
-                            bVal match {
-                                case JsNull => JsNull
-                                case w: JsValue => w
-                            }
-                        }
-                        case v: JsNumber => v
-                        case v: JsString => v
-                        case v: JsObject => {
-                            // Recurse
-                            bVal match {
-                                case w: JsObject => mergeJson(v, w)
-                                case w: JsValue => v
-                            }
-                        }
-                        case v: JsArray => v
-                    }
-                } else aVal
-            }
-        }).toMap ++ bSet.filter(el => !a.keys.contains(el._1))).as[JsObject]
-    }
 }
