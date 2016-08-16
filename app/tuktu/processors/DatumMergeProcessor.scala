20ea997cb5bbf1dabb64782c5a89c07f627eee62
Erik Tromp <e.t.tromp@gmail.com>
2016-08-16 16:55:10 +0200

    Forgot to add the DatumMergeProcessor

---
+++ app/tuktu/processors/DatumMergeProcessor.scala
@@ -0,0 +1,18 @@
+package tuktu.processors
+
+import play.api.libs.json.JsObject
+import play.api.libs.iteratee.Enumeratee
+import tuktu.api.BaseProcessor
+import scala.concurrent.Future
+import tuktu.api.DataPacket
+import scala.concurrent.ExecutionContext.Implicits.global
+import tuktu.api.utils
+
+/**
+ * Merges fields of separate datums into one and returns a single datum with the merged fields in it
+ */
+class DatumMergeProcessor(resultName: String) extends BaseProcessor(resultName) {
+    override def processor: Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
+        new DataPacket(List(data.data.foldLeft(Map.empty[String, Any])((a, b) => utils.mergeMap(a, b))))
+    })
+}
