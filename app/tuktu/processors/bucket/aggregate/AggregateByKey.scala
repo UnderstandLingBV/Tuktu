e4178d2e6424c564ebc62545b2bca62a0de973ca
Erik Tromp <e.t.tromp@gmail.com>
2016-08-23 11:31:26 +0200

    Aggregate by key now aggregates by value and actually works

--- app/tuktu/processors/bucket/aggregate/AggregateByKey.scala
+++ app/tuktu/processors/bucket/aggregate/AggregateByKey.scala
@@ -3,33 +3,61 @@ package tuktu.processors.bucket.aggregate
 import play.api.libs.json.JsObject
 import tuktu.processors.bucket.BaseBucketProcessor
 import tuktu.utils.TuktuArithmeticsParser
+import tuktu.api.utils
+import tuktu.utils.ArithmeticParser
+import play.api.libs.json.JsString
 
 /**
  * Aggregates by key for a given set of keys and an arithmetic expression to compute
- * (can contain aggregation functions
+ * (can contain aggregation functions)
  */
-class AggregateByKeyProcessor(resultName: String) extends BaseBucketProcessor(resultName) {
-    var fields: List[String] = _
+class AggregateByValueProcessor(resultName: String) extends BaseBucketProcessor(resultName) {
+    var fields: List[JsObject] = _
     var expression: String = _
 
     override def initialize(config: JsObject) {
-        fields = (config \ "fields").as[List[String]]
+        fields = (config \ "fields").as[List[JsObject]]
         expression = (config \ "expression").as[String]
     }
 
     override def doProcess(data: List[Map[String, Any]]): List[Map[String, Any]] = {
         if (data.size == 0) List()
         else {
-            val parser = new TuktuArithmeticsParser(data)
             // Go over all paths
-            List((for (field <- fields) yield {
-                // Peplace functions with field names
-                val newExpression = parser.allowedFunctions.foldLeft(expression)((a, b) => {
-                    a.replace(b + "()", b + "(" + field + ")")
-                })
-                // Evaluate string
-                field -> parser(newExpression)
-            }).toMap)
+            (for (obj <- fields) yield {
+                val field = (obj \ "field").as[String]
+                val base = (obj \ "base_value").as[String]
+                
+                // We have to get all the values for this key over all data
+                val baseValues = data.collect {
+                    case m: Map[String, Any] if utils.fieldParser(m, field) != None => {
+                        // Get value to use
+                        val value = utils.fieldParser(m, field).get match {
+                            case v: JsString => v.value
+                            case v: Any => v.toString
+                        }
+                        Map(value.toString ->
+                            ArithmeticParser(tuktu.api.utils.evaluateTuktuString(base, m)))
+                    }
+                }
+                
+                // Create the parse for this field
+                val parser = new TuktuArithmeticsParser(baseValues)
+                
+                // Get all values
+                val allValues = baseValues.flatMap(_.keys).distinct
+               
+                // Compute stuff
+                (for (value <- allValues) yield {
+                    // Peplace functions with field value names
+                    val newExpression = parser.allowedFunctions.foldLeft(expression)((a, b) => {
+                        a.replace(b + "()", b + "(" + value + ")")
+                    })
+                    
+                    // Evaluate string
+                    value -> parser(newExpression)
+                }).toMap
+            }).toList
         }
     }
 }
