420c0faa500455607caa4d455e0e51eb748410cb
Erik Tromp <e.t.tromp@gmail.com>
2016-08-23 12:44:06 +0200

    Renamed concurrent version of AggregateByKey

--- app/tuktu/processors/bucket/concurrent/aggregate/AggregateByKey.scala
+++ app/tuktu/processors/bucket/concurrent/aggregate/AggregateByKey.scala
@@ -7,12 +7,12 @@ import play.api.libs.json.JsObject
 /**
  * Concurrently aggregates by key. Only works for commutative and associative functions (ie. (a+b)+c = a+(b+c) and a+b=b+a) 
  */
-class AggregateByKeyProcessor(genActor: ActorRef, resultName: String) extends BaseConcurrentProcessor(genActor, resultName) {
+class AggregateByValueProcessor(genActor: ActorRef, resultName: String) extends BaseConcurrentProcessor(genActor, resultName) {
     override def initialize(config: JsObject) {
         // Initialize
         this.initializeNodes(
             (config \ "nodes").as[List[String]],
-            "tuktu.processors.bucket.aggregate.AggregateByKeyProcessor",
+            "tuktu.processors.bucket.aggregate.AggregateByValueProcessor",
             config,
             null,
             true)
