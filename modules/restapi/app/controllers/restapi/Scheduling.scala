534a15ee120c417f73f024341b9de9f6fbd6c1fb
NTPape <NTPape@users.noreply.github.com>
2016-09-02 10:37:15 +0200

    Fixed a bug with posting persistent cron schedules through the API

--- modules/restapi/app/controllers/restapi/Scheduling.scala
+++ modules/restapi/app/controllers/restapi/Scheduling.scala
@@ -12,8 +12,7 @@ import play.api.Play
 import play.api.Play.current
 import play.api.cache.Cache
 import play.api.libs.concurrent.Akka
-import play.api.libs.json.JsObject
-import play.api.libs.json.Json
+import play.api.libs.json.{ Json, JsArray, JsObject }
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import tuktu.api.DispatchRequest
@@ -24,16 +23,16 @@ import tuktu.api.scheduler.SimpleScheduler
 
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets.UTF_8
 
 import scala.collection.JavaConversions._
 
 object Scheduling extends Controller {
     implicit val timeout = Timeout(Cache.getAs[Int]("timeout").getOrElse(5) seconds)
-    
+
     // Retrieve the TuktuScheduler
     val scheduler = Akka.system.actorSelection("user/TuktuScheduler")
-    
+
     /**
      * Overview of scheduled jobs
      */
@@ -52,7 +51,7 @@ object Scheduling extends Controller {
             ))
         )
     }
-    
+
     /**
      * Terminate a scheduled job by name
      */
@@ -63,59 +62,60 @@ object Scheduling extends Controller {
             Ok(Json.obj())
         }
     }
-    
+
     /**
      * Schedule (multiple) simple jobs
      */
     def simple()  = Action.async { implicit request =>
         Future {
             val json = request.body.asJson.getOrElse(Json.arr()).as[List[JsObject]]
-            
+
             json.foreach(job => {
                 // Get parameters we need
                 val initialDelay = Duration((job \ "initial_delay").as[String]).asInstanceOf[FiniteDuration]
                 val interval = Duration((job \ "interval").as[String]).asInstanceOf[FiniteDuration]
                 val dispatchRequest = new DispatchRequest((job \ "config").as[String], None, false, false, false, None)     
-            
+
                 scheduler ! new SimpleScheduler((job \ "name").as[String], initialDelay, interval, dispatchRequest)
             })
-            
+
             Ok(Json.obj())
         }
     }
-    
+
     /**
      * Schedules (multiple) cron jobs
      */
     def cron()  = Action.async { implicit request =>
         Future { 
-            val json = request.body.asJson.getOrElse(Json.arr()).as[List[JsObject]]
-            
-            json.foreach(job => {
+            val json = request.body.asJson.map(_.asOpt[List[JsObject]]).flatten.getOrElse(Nil)
+
+            for (job <- json) {
                 // Start cron job
                 val dispatchRequest = new DispatchRequest((job \ "config").as[String], None, false, false, false, None)     
                 scheduler ! new CronScheduler((job \ "name").as[String], (job \ "schedule").as[String], dispatchRequest)
-                
+
                 // Should we add this to the auto start or not?
-                if ((job \ "persist").as[Boolean]) {
+                if ((job \ "persist").asOpt[Boolean].getOrElse(false)) {
                     // Get the autostart cache
                     val autostart = Cache.getAs[List[JsObject]]("tuktu.scheduler.autostart").getOrElse(List())
                     val path = Paths.get(Play.current.configuration.getString("tuktu.autostart").getOrElse("conf/autostart.json"))
-                    
+
                     // Define JSON
                     val json = Json.obj(
                             "id" -> (job \ "config").as[String],
                             "cron" -> (job \ "schedule").as[String]
                     )
                     // Write out
-                    val content = List(Json.obj("autostart" -> Json.arr(autostart ++ List(json))).toString)
-                    Files.write(path, content, Charset.forName("UTF-8"))
-                    
+                    val content = Json.prettyPrint(Json.obj("autostart" -> new JsArray(autostart ++ List(json))))
+
+                    Files.write(path, content.getBytes(UTF_8))
+
                     // Add to cache
                     Cache.set("tuktu.scheduler.autostart", autostart ++ List(json))
                 }
-            })
-            Ok(Json.obj())
+            }
+            Ok
         }
     }
 }
