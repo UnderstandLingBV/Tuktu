a782ef7d092fddc7ec507d2aa8502a5148e32dcd
ErikTromp <e.t.tromp@gmail.com>
2016-10-10 21:40:27 +0200

    Tests are now loaded dynamically from application.conf -> easier to extend with custom suits

--- test/tuktu/test/TuktuTests.scala
+++ test/tuktu/test/TuktuTests.scala
@@ -1,16 +1,25 @@
 package tuktu.test
 
-import org.scalatest.Suites
+import org.scalatest.DoNotDiscover
 import org.scalatest.SequentialNestedSuiteExecution
-import tuktu.test.processor.tests.BaseProcessorTestSuite
-import tuktu.test.processor.tests.BufferProcessorTestSuite
-import tuktu.test.flow.tests.FlowTests
-import org.scalatestplus.play.PlaySpec
+import org.scalatest.Suites
 import org.scalatestplus.play.OneAppPerSuite
-import play.api.libs.concurrent.Akka
+import org.scalatestplus.play.PlaySpec
+import scala.collection.JavaConversions._
+import com.typesafe.config.ConfigFactory
 
 class TuktuTests extends Suites(
-    new BaseProcessorTestSuite,
-    new BufferProcessorTestSuite,
-    new FlowTests
+    new NoTests +:
+    new NoTests +:
+    ({
+        // Get classes that contain the tests, ugly, but hey
+        val testClasses = ConfigFactory.load("application.conf").getStringList("tuktu.tests").toSeq
+        testClasses.map(tc => {
+            val clazz = Class.forName(tc)
+            clazz.getConstructor().newInstance().asInstanceOf[PlaySpec]
+        })
+    }): _*
 ) with SequentialNestedSuiteExecution with OneAppPerSuite
+
+@DoNotDiscover
+class NoTests extends PlaySpec {}
