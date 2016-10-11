927c41853c6ba7f7c6d47ca4ce28e20c4fdcce16
NTPape <NTPape@users.noreply.github.com>
2016-10-11 12:03:39 +0200

    Do Parsing right + tests

--- app/tuktu/utils/Parsing.scala
+++ app/tuktu/utils/Parsing.scala
@@ -1,11 +1,10 @@
 package tuktu.utils
 
 import fastparse.WhitespaceApi
-import play.api.libs.json.JsValue
-import play.api.libs.json.JsObject
+import play.api.libs.json.{ JsArray, JsObject, JsString, JsNull, JsValue }
 import tuktu.api.utils.fieldParser
 import tuktu.processors.bucket.statistics.StatHelper
-import scala.util.{ Try, Success, Failure }
+import scala.util.Try
 
 /**
  * Performs arithmetics over a string representation
@@ -20,201 +19,32 @@ object ArithmeticParser {
 
     // Allow all sorts of numbers, negative and scientific notation
     val number: P[Double] = P(
-            (CharIn(List('-')) ~ CharIn(('0' to '9').toList).rep(1)).!.map(_.toDouble) |
-            CharIn('.' :: 'e' :: ('0' to '9').toList).rep(1).!.map(_.toDouble)
-    )
+        (
+            // If we have a dot, we don't necessarily need a number before the dot
+            ("-".? ~ CharIn('0' to '9').rep ~ "." ~ CharIn('0' to '9').rep(min = 1) |
+                // Otherwise, we need a number
+                "-".? ~ CharIn('0' to '9').rep(min = 1))
+                ~ ("e" ~ "-".? ~ CharIn('0' to '9').rep(min = 1)).?).! map { _.toDouble })
     val parens: P[Double] = P("(" ~/ addSub ~ ")")
-    val factor: P[Double] = P(number | parens)
+    val factor: P[Double] = P(parens | number)
 
     val divMul: P[Double] = P(factor ~ (CharIn("*/").! ~/ factor).rep).map(eval)
     val addSub: P[Double] = P(divMul ~ (CharIn("+-").! ~/ divMul).rep).map(eval)
-    val expr: P[Double] = P((addSub | factor) ~ End)
+    val expr: P[Double] = P(Start ~/ addSub ~ End)
 
-    def eval(tree: (Double, Seq[(String, Double)])) = {
+    def eval(tree: (Double, Seq[(String, Double)])): Double = {
         val (base, ops) = tree
         ops.foldLeft(base) {
             case (left, (op, right)) => op match {
-                case "+" => left + right case "-" => left - right
-                case "*" => left * right case "/" => left / right
+                case "+" => left + right
+                case "-" => left - right
+                case "*" => left * right
+                case "/" => left / right
             }
         }
     }
 
-    def apply(str: String) = {
-        expr.parse(str).get.value
-    }
-}
-
-/**
- * Parses Boolean predicates
- */
-object PredicateParser {
-    val White = WhitespaceApi.Wrapper {
-        import fastparse.all._
-        NoTrace(" ".rep)
-    }
-    import fastparse.noApi._
-    import White._
-
-    // Boolean base logic
-    val literal: P[Boolean] = P(("true" | "false").!.map(_.toBoolean))
-    val parens: P[Boolean] = P("(" ~/ (andOr | arithExpr) ~ ")")
-    val neg: P[Boolean] = P(("!(" ~/ andOr ~ ")") | ("!" ~/ literal)).map(!_)
-    val factor: P[Boolean] = P(litExpr | literal | neg | parens | arithExpr | stringExpr)
-    // Strings
-    val strings: P[String] = P(CharIn(('a' to 'z').toList ++ ('A' to 'Z').toList).rep(1).!.map(_.toString))
-
-    // Evaluate arithmetic expressions on numbers using the ArithmeticParser
-    val arithExpr: P[Boolean] = P((ArithmeticParser.addSub | ArithmeticParser.parens) ~/ ("<" | ">" | ">=" | "<=" | "==" | "!=").! ~ (ArithmeticParser.addSub | ArithmeticParser.parens))
-        .map {
-            case (left, op, right) => op match {
-                case "<" => left < right case ">" => left > right
-                case "<=" => left <= right case ">=" => left >= right
-                case "==" => left == right case "!=" => left != right
-            }
-        }
-    
-    // Evaluate literal expressions
-    val litExpr: P[Boolean] = P(literal ~/ ("==" | "!=").! ~ literal)
-      .map {
-          case (left, "==", right) => left == right
-          case (left, "!=", right) => left != right
-      }
-
-    // Evaluate string expressions
-    val stringExpr: P[Boolean] = P(strings ~/ ("==" | "!=").! ~ strings)
-        .map {
-            case (left, "==", right) => left == right
-            case (left, "!=", right) => left != right
-        }
-
-    // Logical and/or
-    val andOr: P[Boolean] = P(factor ~ (("&&" | "||").! ~/ factor).rep).map(eval)
-    val expr: P[Boolean] = P(andOr ~ End)
-
-    def eval(tree: (Boolean, Seq[(String, Boolean)])) = {
-        val (base, ops) = tree
-        ops.foldLeft(base) {
-            case (left, (op, right)) => op match {
-                case "&&" => left && right case "||" => left || right
-            }
-        }
-    }
-
-    def apply(str: String) = {
-        expr.parse(str).get.value
-    }
-}
-
-/**
- * Parses Boolean predicates over a datum
- */
-class TuktuPredicateParser(datum: Map[String, Any]) {
-    val White = WhitespaceApi.Wrapper {
-        import fastparse.all._
-        NoTrace(" ".rep)
-    }
-    import fastparse.noApi._
-    import White._
-
-    // Strings
-    val strings: P[String] = P((
-            CharIn(('a' to 'z').toList ++ ('A' to 'Z').toList).rep(1) ~
-            CharIn(('a' to 'z').toList ++ ('A' to 'Z').toList ++ List('_', '-', '.', ',', '/') ++ ('0' to '9').toList).rep(0) 
-        ).!.map(_.toString))
-    // All Tuktu-defined functions
-    val functions: P[Boolean] = P(
-            (
-                    (
-                        "containsFields(" | "isNumeric(" | "isNull(" | "isJSON(" | "containsSubstring(" | "isEmptyValue("
-                    ).! ~/ strings ~ ")"
-            ) | (
-                    ("isEmpty(".! ~/ ")".!)
-            )
-        ).map {
-            case ("containsFields(", fields) => fields.split(',').forall { path =>
-                // Get the path and evaluate it against the datum
-                fieldParser(datum, path).isDefined
-            }
-            case ("isNumeric(", field) => Try {
-                    StatHelper.anyToDouble(datum(field))
-                } match {
-                    case Success(double) => true
-                    case Failure(_throw) => false
-                }
-            case ("isNull(", field) => datum(field) match {
-                case null => true
-                case _    => false
-            }
-            case ("isJSON(", fields) => {
-                fields.split(',').forall { path =>
-                    // Get the path, evaluate it against the datum, and check if it's JSON
-                    val result = fieldParser(datum, path)
-                    result.map { res => res.isInstanceOf[JsValue] }.getOrElse(false)
-                }
-            }
-            case ("containsSubstring(", field) => {
-                // Get the actual string and the substring
-                val split = field.split(",")
-                val string = split(0)
-                val substring = split(1)
-                string.contains(substring)
-            }
-            case ("isEmptyValue(", field) => fieldParser(datum, field) match {
-                case None => false
-                case Some(value) => value match {
-                    case a: Seq[_] => a.isEmpty
-                    case a: Map[_, _] => a.isEmpty
-                    case a: String => a.isEmpty
-                    case a: Any => a.toString.isEmpty
-                }
-            }
-            case ("isEmpty(", ")") => datum.isEmpty
-        }
-    // Boolean base logic
-    val literal: P[Boolean] = P(("true" | "false").!.map(_.toBoolean))
-    val parens: P[Boolean] = P("(" ~/ (andOr | arithExpr) ~ ")")
-    val neg: P[Boolean] = P(("!(" ~/ andOr ~ ")") | ("!" ~/ literal)).map(!_)
-    val factor: P[Boolean] = P(litExpr | literal | neg | parens | functions | stringExpr | arithExpr)
-
-    // Evaluate arithmetic expressions on numbers using the ArithmeticParser
-    val arithExpr: P[Boolean] = P((ArithmeticParser.addSub | ArithmeticParser.parens) ~/ ("<" | ">" | ">=" | "<=" | "==" | "!=").! ~ (ArithmeticParser.addSub | ArithmeticParser.parens))
-        .map {
-            case (left, op, right) => op match {
-                case "<" => left < right case ">" => left > right
-                case "<=" => left <= right case ">=" => left >= right
-                case "==" => left == right case "!=" => left != right
-            }
-        }
-    
-    // Evaluate literal expressions
-    val litExpr: P[Boolean] = P(literal ~/ ("==" | "!=").! ~ literal)
-      .map {
-          case (left, "==", right) => left == right
-          case (left, "!=", right) => left != right
-      }
-
-    // Evaluate string expressions
-    val stringExpr: P[Boolean] = P(strings ~/ ("==" | "!=").! ~ strings)
-        .map {
-            case (left, "==", right) => left == right
-            case (left, "!=", right) => left != right
-        }
-
-    // Logical and/or
-    val andOr: P[Boolean] = P(factor ~ (("&&" | "||").! ~/ factor).rep).map(eval)
-    val expr: P[Boolean] = P(andOr ~ End)
-
-    def eval(tree: (Boolean, Seq[(String, Boolean)])) = {
-        val (base, ops) = tree
-        ops.foldLeft(base) {
-            case (left, (op, right)) => op match {
-                case "&&" => left && right case "||" => left || right
-            }
-        }
-    }
-
-    def apply(str: String): Boolean = {
+    def apply(str: String): Double = {
         expr.parse(str).get.value
     }
 }
@@ -231,20 +61,18 @@ class TuktuArithmeticsParser(data: List[Map[String, Any]]) {
     import White._
 
     // List of allowed functions
-    def allowedFunctions = List("count", "avg", "median", "sum", "max", "min", "stdev")
+    val allowedFunctions = List("count", "avg", "median", "sum", "max", "min", "stdev")
 
     // All Tuktu-defined arithmetic functions
     val functions: P[Double] = P(
-            StringIn(allowedFunctions: _*).! ~/ "(" ~/ CharPred(_ != ')').rep(0).! ~ ")"
-        ).map {
+        StringIn(allowedFunctions: _*).! ~/ "(" ~/ CharPred(_ != ')').rep(0).! ~ ")").map {
             case ("avg", field) => {
                 val (sum, count) = data.foldLeft(0.0, 0) {
                     case ((sum, count), datum) =>
                         val v = fieldParser(datum, field).map { StatHelper.anyToDouble(_) }
                         (
                             sum + v.getOrElse(0.0),
-                            count + { if (v.isDefined) 1 else 0 }
-                        )
+                            count + { if (v.isDefined) 1 else 0 })
                 }
 
                 if (count > 0)
@@ -294,26 +122,160 @@ class TuktuArithmeticsParser(data: List[Map[String, Any]]) {
             }
         }
 
-    // Allow all sorts of numbers, negative and scientific notation
-    val number: P[Double] = P(CharIn('-' :: '.' :: 'e' :: ('0' to '9').toList).rep(1).!.map(_.toDouble))
     val parens: P[Double] = P("(" ~/ addSub ~ ")")
-    val factor: P[Double] = P(number | parens | functions)
+    val factor: P[Double] = P(parens | ArithmeticParser.number | functions)
 
-    val divMul: P[Double] = P(factor ~ (CharIn("*/").! ~/ factor).rep).map(eval)
-    val addSub: P[Double] = P(divMul ~ (CharIn("+-").! ~/ divMul).rep).map(eval)
-    val expr: P[Double] = P((addSub | factor) ~ End)
+    val divMul: P[Double] = P(factor ~ (CharIn("*/").! ~/ factor).rep).map(ArithmeticParser.eval)
+    val addSub: P[Double] = P(divMul ~ (CharIn("+-").! ~/ divMul).rep).map(ArithmeticParser.eval)
+    val expr: P[Double] = P(Start ~/ addSub ~ End)
 
-    def eval(tree: (Double, Seq[(String, Double)])) = {
+    def apply(str: String): Double = {
+        expr.parse(str).get.value
+    }
+}
+
+/**
+ * Parses Boolean predicates
+ */
+object PredicateParser {
+    val White = WhitespaceApi.Wrapper {
+        import fastparse.all._
+        NoTrace(" ".rep)
+    }
+    import fastparse.noApi._
+    import White._
+
+    // Boolean base logic
+    def negate(tuple: (String, Boolean)): Boolean = {
+        if (tuple._1.size % 2 == 0)
+            tuple._2
+        else
+            !tuple._2
+    }
+    val literal: P[Boolean] = P("!".rep.! ~ ("true" | "false").!).map { case (neg, pred) => negate((neg, pred.toBoolean)) }
+
+    // Evaluate arithmetic expressions on numbers using the ArithmeticParser
+    val arithExpr: P[Boolean] = P(ArithmeticParser.addSub ~ ("<" | ">" | ">=" | "<=" | "==" | "!=").! ~ ArithmeticParser.addSub)
+        .map {
+            case (left, op, right) => op match {
+                case "<"  => left < right
+                case ">"  => left > right
+                case "<=" => left <= right
+                case ">=" => left >= right
+                case "==" => left == right
+                case "!=" => left != right
+            }
+        }
+
+    // Evaluate string expressions
+    val strings: P[String] = P((
+        CharIn(('a' to 'z') ++ ('A' to 'Z') ++ "_-.,/\"'") ~
+        CharIn(('a' to 'z') ++ ('A' to 'Z') ++ "_-.,/\"'" ++ ('0' to '9')).rep).!)
+    val stringExpr: P[Boolean] = P(strings ~ ("==" | "!=").! ~ strings)
+        .map {
+            case (left, "==", right) => left == right
+            case (left, "!=", right) => left != right
+        }
+
+    val basePredicate: P[Boolean] = (literal | arithExpr | stringExpr)
+
+    val and: P[Boolean] = P(factor ~ ("&&".! ~/ factor).rep).map(eval)
+    val or: P[Boolean] = P(and ~ ("||".! ~/ and).rep).map(eval)
+
+    val parens: P[Boolean] = P("!".rep.! ~ "(" ~ or ~ ")").map(negate)
+    val factor: P[Boolean] = P(parens | basePredicate)
+
+    val expr: P[Boolean] = P(Start ~/ or ~ End)
+
+    def eval(tree: (Boolean, Seq[(String, Boolean)])): Boolean = {
         val (base, ops) = tree
         ops.foldLeft(base) {
             case (left, (op, right)) => op match {
-                case "+" => left + right case "-" => left - right
-                case "*" => left * right case "/" => left / right
+                case "&&" => left && right
+                case "||" => left || right
             }
         }
     }
 
-    def apply(str: String) = {
+    def apply(str: String): Boolean = {
+        expr.parse(str).get.value
+    }
+}
+
+/**
+ * Parses Boolean predicates over a datum
+ */
+class TuktuPredicateParser(datum: Map[String, Any]) {
+    val White = WhitespaceApi.Wrapper {
+        import fastparse.all._
+        NoTrace(" ".rep)
+    }
+    import fastparse.noApi._
+    import White._
+
+    // All Tuktu-defined functions
+    val allowedFunctions: List[String] = List("containsFields", "isNumeric", "isNull", "isJSON", "containsSubstring", "isEmptyValue")
+    val functions: P[Boolean] = P(((StringIn(allowedFunctions: _*).! ~ "(" ~/ CharPred(_ != ')').rep.! ~/ ")"))).map {
+        case ("containsFields", fields) => fields.split(',').forall { path =>
+            // Get the path and evaluate it against the datum
+            fieldParser(datum, path).isDefined
+        }
+        case ("isNumeric", field) => Try {
+            StatHelper.anyToDouble(fieldParser(datum, field).get)
+        }.isSuccess
+        case ("isNull", field) => fieldParser(datum, field) match {
+            case Some(null)   => true
+            case Some(JsNull) => true
+            case _            => false
+        }
+        case ("isJSON", fields) => {
+            fields.split(',').forall { path =>
+                // Get the path, evaluate it against the datum, and check if it's JSON
+                fieldParser(datum, path).flatMap { res =>
+                    if (res.isInstanceOf[JsValue])
+                        res.asInstanceOf[JsValue].asOpt[JsValue].map { _ => true }
+                    else
+                        None
+                }.getOrElse(false)
+            }
+        }
+        case ("containsSubstring", field) => {
+            // Get the actual string and the substring
+            val split = field.split(",")
+            val string = split(0)
+            val substring = split(1)
+            string.contains(substring)
+        }
+        case ("isEmptyValue", field) => fieldParser(datum, field) match {
+            case None => false
+            case Some(value) => value match {
+                case a: TraversableOnce[_] => a.isEmpty
+                case a: String             => a.isEmpty
+                case a: JsArray            => a.value.isEmpty
+                case a: JsObject           => a.value.isEmpty
+                case a: JsString           => a.value.isEmpty
+                case a: Any                => a.toString.isEmpty
+            }
+        }
+    }
+    def allowedParameterfreeFunctions: List[String] = List("isEmpty")
+    val parameterfreeFunctions: P[Boolean] = P(StringIn(allowedParameterfreeFunctions: _*).! ~ "(" ~/ ")").map {
+        case ("isEmpty") => datum.isEmpty
+    }
+    val allFunctions: P[Boolean] = P("!".rep.! ~ (functions | parameterfreeFunctions)).map(PredicateParser.negate)
+
+    // Boolean base logic
+    val basePredicate: P[Boolean] = (allFunctions | PredicateParser.literal | PredicateParser.arithExpr | PredicateParser.stringExpr)
+
+    val and: P[Boolean] = P(factor ~ ("&&".! ~/ factor).rep).map(PredicateParser.eval)
+    val or: P[Boolean] = P(and ~ ("||".! ~/ and).rep).map(PredicateParser.eval)
+
+    val parens: P[Boolean] = P("!".rep.! ~ "(" ~ or ~ ")").map(PredicateParser.negate)
+    val factor: P[Boolean] = P(parens | basePredicate)
+
+    val expr: P[Boolean] = P(Start ~/ or ~ End)
+
+    def apply(str: String): Boolean = {
         expr.parse(str).get.value
     }
 }
