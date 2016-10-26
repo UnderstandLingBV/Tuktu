e9f985d71488aefe40fd1f38b86e42cbaefc77f6
ErikTromp <e.t.tromp@gmail.com>
2016-10-26 11:39:11 +0200

    Fix in release of SQL conn with generator

--- modules/nosql/app/tuktu/nosql/generators/sql.scala
+++ modules/nosql/app/tuktu/nosql/generators/sql.scala
@@ -13,6 +13,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class SQLGenerator(resultName: String, processors: List[Enumeratee[DataPacket, DataPacket]], senderActor: Option[ActorRef]) extends BaseGenerator(resultName, processors, senderActor) {
+    var connDef: ConnectionDefinition = _
+    var connection: Connection = _
+    
     override def _receive = {
         case config: JsValue => {
             // Get url, username and password for the connection; and the SQL driver (new drivers may have to be added to dependencies) and query
@@ -26,14 +29,13 @@ class SQLGenerator(resultName: String, processors: List[Enumeratee[DataPacket, D
             val flatten = (config \ "flatten").asOpt[Boolean].getOrElse(false)
 
             // Load the driver, set up the client
-            val connDef = ConnectionDefinition(url, user, password, driver)
-            val connection = sql.getConnection(connDef)
+            connDef = ConnectionDefinition(url, user, password, driver)
+            connection = sql.getConnection(connDef)
 
             // Build the enumerator to query SQL
             val rowEnumerator = sql.streamResult(query)(connection).andThen(Enumerator.eof)
             // Stop packet upon termination
             val onEOF = Enumeratee.onEOF[Row](() => {
-                sql.releaseConnection(connDef, connection)
                 self ! new StopPacket
             })
             // Enumeratee to turn the Row into a DP
@@ -47,6 +49,9 @@ class SQLGenerator(resultName: String, processors: List[Enumeratee[DataPacket, D
                 rowEnumerator |>> (onEOF compose rowToDP compose processor) &>> sinkIteratee
             })
         }
-        case sp: StopPacket => cleanup(false)
+        case sp: StopPacket => {
+            sql.releaseConnection(connDef, connection)
+            cleanup(false)
+        }
     }
 }
