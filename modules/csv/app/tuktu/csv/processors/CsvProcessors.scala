069ecb3d814eeea828641c2b3705831c8d9d747f
ErikTromp <e.t.tromp@gmail.com>
2016-10-31 15:10:01 +0100

    new in front of DP

--- modules/csv/app/tuktu/csv/processors/CsvProcessors.scala
+++ modules/csv/app/tuktu/csv/processors/CsvProcessors.scala
@@ -83,7 +83,7 @@ class CSVReaderProcessor(resultName: String) extends BaseProcessor(resultName) {
     }
 
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM((data: DataPacket) => Future {
-        DataPacket({
+        new DataPacket({
             // See if this is the first one
             val first = headers == null
 
