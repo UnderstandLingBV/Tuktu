617b4f7bc8c038225cf457363d9e662f00b5f1e0
NTPape <NTPape@users.noreply.github.com>
2017-02-16 19:25:31 +0100

    Remove redundant imports

--- modules/nosql/app/tuktu/nosql/generators/sql.scala
+++ modules/nosql/app/tuktu/nosql/generators/sql.scala
@@ -1,30 +1,26 @@
 package tuktu.nosql.generators
 
-import java.sql._
 import akka.actor.ActorRef
 import play.api.libs.iteratee.Enumeratee
 import play.api.libs.json.JsValue
 import tuktu.api._
+import java.sql.Connection
 import tuktu.nosql.util.sql
 import tuktu.nosql.util.sql.ConnectionDefinition
-import play.api.libs.iteratee.Enumerator
-import anorm.Row
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class SQLGenerator(resultName: String, processors: List[Enumeratee[DataPacket, DataPacket]], senderActor: Option[ActorRef]) extends BaseGenerator(resultName, processors, senderActor) {
     var connDef: ConnectionDefinition = _
     var conn: Connection = _
-    
-    override def receive() = {
-        case config: JsValue => {
+
+    override def _receive = {
+        case config: JsValue =>
             // Get url, username and password for the connection; and the SQL driver (new drivers may have to be added to dependencies) and query
             val url = (config \ "url").as[String]
             val user = (config \ "user").as[String]
             val password = (config \ "password").as[String]
             val query = (config \ "query").as[String]
             val driver = (config \ "driver").as[String]
-            
+
             // Do we need to flatten or not?
             val flatten = (config \ "flatten").asOpt[Boolean].getOrElse(false)
 
@@ -32,19 +28,18 @@ class SQLGenerator(resultName: String, processors: List[Enumeratee[DataPacket, D
             connDef = ConnectionDefinition(url, user, password, driver)
             conn = sql.getConnection(connDef)
 
-            // Run the query
+            // Run the query and push the results
             val rows = sql.queryResult(query)(conn)
-            for (row <- rows) flatten match {
-                case true => channel.push(new DataPacket(List(row)))
-                case false => channel.push(new DataPacket(List(Map(resultName -> row))))
-            }
+            if (flatten)
+                for (row <- rows) channel.push(DataPacket(List(row)))
+            else
+                for (row <- rows) channel.push(DataPacket(List(Map(resultName -> row))))
 
             // We stop once the query is done
             self ! new StopPacket
-        }
-        case sh: StopPacket => {
+
+        case sh: StopPacket =>
             sql.releaseConnection(connDef, conn)
             cleanup
-        }
     }
 }
--- modules/nosql/app/tuktu/nosql/util/sql.scala
+++ modules/nosql/app/tuktu/nosql/util/sql.scala
@@ -1,30 +1,10 @@
 package tuktu.nosql.util
 
 import java.sql.Connection
-import java.util.UUID
-import java.util.concurrent.Semaphore
-
-import scala.Right
-import scala.collection.concurrent.TrieMap
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import com.jolbox.bonecp.BoneCP
-import com.jolbox.bonecp.BoneCPConfig
-
-import anorm.NamedParameter
-import anorm.ParameterValue.toParameterValue
-import anorm.Row
-import anorm.RowParser
-import anorm.SQL
-import anorm.SqlParser
-import anorm.SqlStringInterpolation
-import anorm.sqlToSimple
-import play.api.libs.iteratee.Enumerator
-import akka.actor.Actor
-import scala.util.Random
+import com.zaxxer.hikari.{ HikariConfig, HikariDataSource }
+import anorm._
 import play.api.Play
-import com.zaxxer.hikari.HikariDataSource
-import com.zaxxer.hikari.HikariConfig
+import scala.Right
 
 /**
  * Keeps track of connections
@@ -35,7 +15,7 @@ object sql {
         user: String,
         password: String,
         driver: String)
-        
+
     val pools = collection.mutable.Map[ConnectionDefinition, HikariDataSource]()
     // Gets a single connection
     def getConnection(conn: ConnectionDefinition,
@@ -53,11 +33,11 @@ object sql {
             pools += conn -> ds
             ds.getConnection
         }
-        
+
         // Check if a pool exists
         if (pools.contains(conn)) pools(conn).getConnection
         else
-             // Create the pool and return one
+            // Create the pool and return one
             newConnection
     }
     // Releases a connection, and closes its BasicDataSource if it has no more active connections
