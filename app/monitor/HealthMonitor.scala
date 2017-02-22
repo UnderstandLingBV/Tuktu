54fbe0511d03b9d9639458ced6a764c4f82c82a1
NTPape <NTPape@users.noreply.github.com>
2017-02-22 11:57:43 +0100

    Fixed bug in HealthMonitor's HealthRound

--- app/monitor/HealthMonitor.scala
+++ app/monitor/HealthMonitor.scala
@@ -14,14 +14,13 @@ import tuktu.api.ClusterNode
 import tuktu.api.HealthCheck
 import tuktu.api.HealthReply
 import play.api.Play
+import scala.util.{ Try, Success, Failure }
 
 case class HealthRound()
 case class AddNode(
-        node: ClusterNode
-)
+    node: ClusterNode)
 case class DeleteNode(
-        node: String
-)
+    node: String)
 
 /**
  * Checks the health of the cluster; pings all dispatchers every now and then to see if they
@@ -29,67 +28,52 @@ case class DeleteNode(
  */
 class HealthMonitor() extends Actor with ActorLogging {
     implicit val timeout = Timeout(Cache.getAs[Int]("timeout").getOrElse(5) seconds)
-    
+
     // Keep track of how many times we sent a failed message to each node
-    val healthChecks = collection.mutable.Map[String, Int]()
-    val initcNodes = Cache.getOrElse[scala.collection.mutable.Map[String, ClusterNode]]("clusterNodes")(scala.collection.mutable.Map())
-    val homeAddress = Cache.getAs[String]("homeAddress").getOrElse("127.0.0.1")
-    initcNodes.foreach(node => {
-        if (node._1 != homeAddress)
-            healthChecks += node._1 -> 0
-    })
-    
+    val healthChecks = collection.mutable.Map.empty[String, Int].withDefault { _ => 0 }
+
     // Get maximum number of failures
     val maxFails = Cache.getAs[Int]("mon.max_health_fails").getOrElse(Play.current.configuration.getInt("tuktu.monitor.max_health_fails").getOrElse(3))
-    
+
     // Set up periodic health checks
     val interval = Cache.getAs[Int]("mon.health_interval").getOrElse(Play.current.configuration.getInt("tuktu.monitor.health_interval").getOrElse(30))
     Akka.system.scheduler.schedule(
-            interval seconds,
-            interval seconds,
-            self,
-            new HealthRound
-    )
-    
-    def receive() = {
-        case hr: HealthRound => {
+        interval seconds,
+        interval seconds,
+        self,
+        new HealthRound)
+
+    def receive = {
+        case hr: HealthRound =>
+            // Get all other nodes
             val cNodes = Cache.getOrElse[scala.collection.mutable.Map[String, ClusterNode]]("clusterNodes")(scala.collection.mutable.Map())
-            
-            // Send out health checks to all nodes
+            val homeAddress = Cache.getAs[String]("homeAddress").getOrElse("127.0.0.1")
             val clusterNodes = cNodes - homeAddress
-            val futures = for ((hostname, node) <- clusterNodes) yield {
-                // Return future
-                val location = "akka.tcp://application@" + hostname  + ":" + node.akkaPort + "/user/TuktuDispatcher"
-                (hostname, (Akka.system.actorSelection(location) ? new HealthCheck).asInstanceOf[Future[HealthReply]])
-            }
-            
-            // Reset counter on success, increase on failure
-            futures.map(future => {
-                future._2.onSuccess {
-                    case hr: HealthReply => healthChecks(future._1) = 0
-                }
-                future._2.onFailure {
-                    case _ => {
+
+            // Send out health checks to all nodes
+            for ((hostname, node) <- clusterNodes) {
+                val location = "akka.tcp://application@" + hostname + ":" + node.akkaPort + "/user/TuktuDispatcher"
+                (Akka.system.actorSelection(location) ? new HealthCheck).onComplete {
+                    case Success(_) =>
+                        // Can remove for now until next failure
+                        healthChecks -= hostname
+                    case Failure(_) =>
                         // Increase counter and see if we need to remove
-                        healthChecks(future._1) += 1
-                        if (healthChecks(future._1) >= maxFails) {
-                            // Remove this node from our cluster
-                            cNodes -= future._1
-                            // Remove from health checks
-                            healthChecks -= future._1
+                        healthChecks(hostname) += 1
+                        if (healthChecks(hostname) >= maxFails) {
+                            // Remove from cluster and from health checks
+                            cNodes -= hostname
+                            healthChecks -= hostname
                         }
-                    }
                 }
-            })
-        }
-        case an: AddNode => {
+            }
+        case AddNode(node) =>
             Cache.getOrElse[scala.collection.mutable.Map[String, ClusterNode]]("clusterNodes")(scala.collection.mutable.Map()) +=
-                an.node.host -> an.node
-                
+                node.host -> node
+
             sender ! "ok"
-        }
-        case dn: DeleteNode =>
-            Cache.getOrElse[scala.collection.mutable.Map[String, ClusterNode]]("clusterNodes")(scala.collection.mutable.Map()) -= dn.node
+        case DeleteNode(node) =>
+            Cache.getOrElse[scala.collection.mutable.Map[String, ClusterNode]]("clusterNodes")(scala.collection.mutable.Map()) -= node
         case _ => {}
     }
 }
