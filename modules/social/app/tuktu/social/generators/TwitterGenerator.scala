64b715c0497680442c1704d5ea090eada03b2069
ErikTromp <e.t.tromp@gmail.com>
2017-04-05 09:53:58 +0200

    Added Twitter sample generator

--- modules/social/app/tuktu/social/generators/TwitterGenerator.scala
+++ modules/social/app/tuktu/social/generators/TwitterGenerator.scala
@@ -88,3 +88,67 @@ class TwitterGenerator(resultName: String, processors: List[Enumeratee[DataPacke
         }
     }
 }
+
+class TwitterSampleGenerator(resultName: String, processors: List[Enumeratee[DataPacket, DataPacket]], senderActor: Option[ActorRef]) extends BaseGenerator(resultName, processors, senderActor) {
+    var twitterStream: TwitterStream = _
+
+    override def _receive = {
+        case config: JsValue => {
+            // Get credentials
+            val credentials = (config \ "credentials").as[JsObject]
+            val consumerKey = (credentials \ "consumer_key").as[String]
+            val consumerSecret = (credentials \ "consumer_secret").as[String]
+            val accessToken = (credentials \ "access_token").as[String]
+            val accessTokenSecret = (credentials \ "access_token_secret").as[String]
+
+            // Implement the Twitter4J status listener which determines what to listen for
+            val listener: StatusListener = new StatusListener() {
+                @Override
+                def onStatus(status: Status): Unit = {
+                    // Flatten the status and push it on
+                    val flatStatus = Json.parse(DataObjectFactory.getRawJSON(status))
+                    channel.push(DataPacket(List(Map(resultName -> flatStatus))))
+                }
+
+                @Override
+                def onDeletionNotice(sdn: StatusDeletionNotice): Unit = {}
+
+                @Override
+                def onTrackLimitationNotice(i: Int): Unit = {}
+
+                @Override
+                def onScrubGeo(l: Long, l1: Long): Unit = {}
+
+                @Override
+                def onException(e: Exception): Unit = {
+                    Logger.error("Exception while creating Twitter steam.", e)
+                }
+
+                @Override
+                def onStallWarning(warning: StallWarning): Unit = {
+                    Logger.warn(warning.toString)
+                }
+            }
+
+            // Create a Twitter4J configuration to pass on the OAuth credentials
+            val cb = new ConfigurationBuilder().
+                setDebugEnabled(true).
+                setJSONStoreEnabled(true).
+                setOAuthConsumerKey(consumerKey).
+                setOAuthConsumerSecret(consumerSecret).
+                setOAuthAccessToken(accessToken).
+                setOAuthAccessTokenSecret(accessTokenSecret)
+            // Create a Twitter4J instance with the OAuth credentials and the listener
+            val fact = new TwitterStreamFactory(cb.build)
+            twitterStream = fact.getInstance
+            twitterStream.addListener(listener)
+
+            // Start collecting
+            twitterStream.sample
+        }
+        case sp: StopPacket => {
+            twitterStream.shutdown
+            cleanup
+        }
+    }
+}
