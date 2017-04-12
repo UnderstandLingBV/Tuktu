1ef8044a4f6cd1a18bff4f405c5ad0eb4afb345a
ErikTromp <e.t.tromp@gmail.com>
2017-04-12 09:55:41 +0200

    Don't redefine tuktuFlow if it was already defined

--- modules/web/app/views/Tuktu.scala.js
+++ modules/web/app/views/Tuktu.scala.js
@@ -12,23 +12,25 @@ function appendScript(link) {
 	h.appendChild(s);
 }
 if (typeof jQuery === 'undefined') {appendScript("//code.jquery.com/jquery-2.2.0.min.js");}
-function tuktuFlow(nf) {
-	var h = document.getElementsByTagName("head")[0];
-	var s = document.createElement("script");
-	s.setAttribute("type", "text/javascript");
-	s.innerHTML = "var xhr = new XMLHttpRequest();" +
-		"xhr.open('POST', '@jsUrl', true);" +
-		"xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');" +
-		"xhr.send(JSON.stringify({f: '" + nf + "', d: tuktuvars}));" +
-		"xhr.onreadystatechange = function() {" +
-			"if (xhr.readyState == 4) {" +
-				"var se = document.createElement('script');" +
-				"se.setAttribute('type', 'text/javascript');" +
-				"se.innerHTML = xhr.responseText;" +
-				"document.getElementsByTagName('head')[0].appendChild(se);" +
-			"}" +
-		"}";
-	h.appendChild(s);
+if (typeof tuktuFlow !== "function") {
+	function tuktuFlow(nf) {
+		var h = document.getElementsByTagName("head")[0];
+		var s = document.createElement("script");
+		s.setAttribute("type", "text/javascript");
+		s.innerHTML = "var xhr = new XMLHttpRequest();" +
+			"xhr.open('POST', '@jsUrl', true);" +
+			"xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');" +
+			"xhr.send(JSON.stringify({f: '" + nf + "', d: tuktuvars}));" +
+			"xhr.onreadystatechange = function() {" +
+				"if (xhr.readyState == 4) {" +
+					"var se = document.createElement('script');" +
+					"se.setAttribute('type', 'text/javascript');" +
+					"se.innerHTML = xhr.responseText;" +
+					"document.getElementsByTagName('head')[0].appendChild(se);" +
+				"}" +
+			"}";
+		h.appendChild(s);
+	}
 }
 var tuktu = function() {
 	tuktuvars.referrer = document.referrer;
