a807ed25520f69035cf67b14e9739cfe02115f78
NTPape <NTPape@users.noreply.github.com>
2017-04-28 11:48:51 +0200

    JSONCreatorProcessor now prepares the json once and then only evaluates

--- app/tuktu/processors/json/JSONCreatorProcessor.scala
+++ app/tuktu/processors/json/JSONCreatorProcessor.scala
@@ -10,15 +10,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * Creates a JSON object
  */
 class JSONCreatorProcessor(resultName: String) extends BaseProcessor(resultName) {
-    var json: JsValue = _
+    var json: utils.PreparedJsNode = _
 
     override def initialize(config: JsObject) {
-        json = (config \ "json").as[JsValue]
+        json = utils.prepareTuktuJsValue((config \ "json").as[JsValue])
     }
 
     override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
         for (datum <- data) yield {
-            datum + (resultName -> utils.evaluateTuktuJsValue(json, datum))
+            datum + (resultName -> json.evaluate(datum))
         }
     })
 }
