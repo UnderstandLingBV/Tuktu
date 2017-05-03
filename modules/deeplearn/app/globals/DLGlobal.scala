c3af23d8715a2ec04f557077755115632df80fb2
ErikTromp <e.t.tromp@gmail.com>
2017-05-03 14:50:13 +0200

    Default VGG16 loading is false

--- modules/deeplearn/app/globals/DLGlobal.scala
+++ modules/deeplearn/app/globals/DLGlobal.scala
@@ -7,6 +7,6 @@ import tuktu.deeplearn.models.image.VGG16
 
 class DLGlobal() extends TuktuGlobal() {
     override def onStart(app: Application) = {
-        if (Play.current.configuration.getBoolean("tuktu.dl.vgg16.load_on_start").getOrElse(true)) VGG16.load
+        if (Play.current.configuration.getBoolean("tuktu.dl.vgg16.load_on_start").getOrElse(false)) VGG16.load
     }
 }
