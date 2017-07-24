80d6d979335459d204a677bbc3149888f9c269f4
NTPape <NTPape@users.noreply.github.com>
2017-07-24 18:13:53 +0200

    Simplify and make AggregateByValueProcessor a lot faster on huge DP

--- app/tuktu/processors/bucket/aggregate/AggregateByValue.scala
+++ app/tuktu/processors/bucket/aggregate/AggregateByValue.scala
@@ -17,58 +17,43 @@ class AggregateByValueProcessor(resultName: String) extends BaseBucketProcessor(
     // First group distinct values within these fields and retain them in the result (since they are all the same); everything else will be dropped
     var group: List[String] = _
     // The base value for each distinct value; for count() this is probably 1; for everything else it probably is the value of the ${field} you want the expression to be executed on
-    var base: String = _
+    var base: utils.evaluateTuktuString.TuktuStringRoot = _
+    // Base key constant used internally
+    val baseKey: String = "b"
     // The expression, most of the time it probably is just min(), max(), count(), etc. (see ArithmeticParser for available functions), but can be combined
-    var expression: String = _
+    var expression: utils.evaluateTuktuString.TuktuStringRoot = _
     var evaluatedExpression: Option[String] = None
 
     override def initialize(config: JsObject) {
         group = (config \ "group").as[List[String]]
-        base = (config \ "base_value").as[String]
-        expression = (config \ "expression").as[String]
+        base = utils.evaluateTuktuString.prepare((config \ "base_value").as[String])
+        expression = utils.evaluateTuktuString.prepare((config \ "expression").as[String])
     }
 
-    private def represent(o: Option[Any]): String = o match {
-        case Some(v: JsString) => v.toString
-        case Some(v: Any)      => JsString(v.toString).toString
-        case None              => "None"
-    }
-
-    /**
-     * @param data A DataPacket's data
-     * @return Cube values (to write to result) and Map(cubeStringRepresentation -> evaluated expression)
-     */
-    private def preprocess(data: List[Map[String, Any]]): List[(List[Option[Any]], Map[String, Any])] = {
-        for (datum <- data) yield {
-            // Evaluate expression
-            if (evaluatedExpression == None)
-                evaluatedExpression = Some(utils.evaluateTuktuString(expression, datum))
-
-            // Get value to use
-            val values = group map { field => utils.fieldParser(datum, utils.evaluateTuktuString(field, datum)) }
-            val jsStrings = values.map(represent)
-            (values, Map(jsStrings.mkString(",") -> ArithmeticParser(utils.evaluateTuktuString(base, datum))))
+    override def processor: Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM { data =>
+        if (evaluatedExpression.isEmpty && data.nonEmpty) {
+            evaluatedExpression =
+                Some(ArithmeticParser.allowedFunctions.foldLeft(
+                    utils.evaluateTuktuString(expression, data.data.head)) {
+                        (a, b) => a.replace(b + "()", b + "(" + JsString(baseKey).toString + ")")
+                    })
         }
+
+        Future { DataPacket(doProcess(data.data)) }
     }
 
-    override def processor(): Enumeratee[DataPacket, DataPacket] = Enumeratee.mapM(data => Future {
-        DataPacket({
-            // We have to get all the values for this key over all data
-            val baseValues = preprocess(data.data)
+    override def doProcess(data: List[Map[String, Any]]): List[Map[String, Any]] = {
+        // TODO: Look into whether this works in a concurrent setting
+        // We have to get all the values for this key over all data
+        val groups = data.map {
+            datum => (Map(baseKey -> ArithmeticParser(base.evaluate(datum))), datum)
+        }.groupBy {
+            case (base, datum) => group.map { key => key -> utils.fieldParser(datum, key) }
+        }
 
-            // Create the parser data for this field
-            val d = baseValues.map(_._2)
-
-            // Get all values
-            val allValues = baseValues map { _._1 } distinct
-
-            // Compute stuff
-            for (values <- allValues) yield {
-                val jsStrings = values.map(represent)
-                // Peplace functions with field value names
-                val newExpression = ArithmeticParser.allowedFunctions.foldLeft(evaluatedExpression.get)((a, b) => {
-                    a.replace(b + "()", b + "(" + JsString(jsStrings.mkString(",")).toString + ")")
-                })
+        groups.map {
+            case (values, list) =>
+                val d = list.map(_._1)
 
                 // Build nested result: split by '.' and nest the whole way down; then mergeMaps
                 def buildResult(tuples: List[(String, Any)], current: Map[String, Any] = Map.empty): Map[String, Any] = tuples match {
@@ -81,27 +66,7 @@ class AggregateByValueProcessor(resultName: String) extends BaseBucketProcessor(
                         buildResult(tail, utils.mergeMap(current, helper(head._1.split('.').toList, head._2)))
                     }
                 }
-                buildResult(group.zip(values) ++ List(resultName -> ArithmeticParser(newExpression, d)))
-            }
-        })
-    })
-
-    override def doProcess(data: List[Map[String, Any]]): List[Map[String, Any]] = {
-        if (data.isEmpty) List()
-        else {
-            // Get all values
-            val allValues = data.flatMap(_.keys).distinct
-
-            // Compute stuff
-            List((for (value <- allValues) yield {
-                // Replace functions with field value names
-                val newExpression = ArithmeticParser.allowedFunctions.foldLeft(expression)((a, b) => {
-                    a.replace(b + "()", b + "(" + value + ")")
-                })
-
-                // Evaluate string
-                value -> ArithmeticParser(newExpression, data)
-            }).toMap)
-        }
+                buildResult((for ((key, Some(value)) <- values) yield key -> value) ++ List(resultName -> ArithmeticParser(evaluatedExpression.get, d)))
+        }.toList
     }
 }
