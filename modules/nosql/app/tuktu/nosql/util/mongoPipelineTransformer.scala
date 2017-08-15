3a6ddc0d71cf473acc461ba6390e5ead0596c419
NTPape <NTPape@users.noreply.github.com>
2017-08-15 18:02:05 +0200

    Simplified the MongoDB PipelineTransformer

--- modules/nosql/app/tuktu/nosql/util/mongoPipelineTransformer.scala
+++ modules/nosql/app/tuktu/nosql/util/mongoPipelineTransformer.scala
@@ -1,139 +1,57 @@
 package tuktu.nosql.util
 
-import play.api.cache.Cache
-
-import play.api.libs.iteratee.Enumeratee
-import play.api.libs.iteratee.Iteratee
-
 import play.api.libs.json._
-import play.api.libs.json.Json._
-
-import play.api.Play.current
-
-import play.modules.reactivemongo.json._
 import play.modules.reactivemongo.json.collection._
-
-import reactivemongo.api.commands.Command
-import reactivemongo.api._
-import reactivemongo.api.commands.AggregationFramework
-
-import scala.collection.Set
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import scala.concurrent.Future
-
 import tuktu.api._
 
 class MongoPipelineTransformer(implicit collection: JSONCollection) {
     def json2task(jobj: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.PipelineOperator = {
-        import collection.BatchCommands.AggregationFramework.{
-            AggregationResult,
-            PipelineOperator,
-            Skip,
-            Limit,
-            Unwind,
-            Out,
-            Sort,
-            SortOrder,
-            Match,
-            Project,
-            Group
+        import collection.BatchCommands.AggregationFramework._
+
+        jobj.value.head match {
+            case ("$skip", JsNumber(n))    => Skip(n.intValue)
+            case ("$limit", JsNumber(n))   => Limit(n.intValue)
+            case ("$unwind", JsString(s))  => Unwind(s) // Unwind prepends field names with '$'
+            case ("$out", JsString(s))     => Out(s)
+            case ("$sort", o: JsObject)    => Sort(getSortOrder(o): _*)
+            case ("$match", o: JsObject)   => Match(o)
+            case ("$project", o: JsObject) => Project(o)
+            case ("$group", o: JsObject)   => getGroup(o)
         }
-
-        val keys: Set[String] = jobj.keys
-
-        val result: PipelineOperator = keys.head match {
-            case "$skip"    => Skip(jobj.\("$skip").as[Int])
-            case "$limit"   => Limit(jobj.\("$limit").as[Int])
-            case "$unwind"  => Unwind(jobj.\("$unwind").as[String]) // Unwind prepends field names with '$'
-            case "$out"     => Out(jobj.\("$out").as[String])
-            case "$sort"    => Sort(getSortOrder(jobj.\("$sort").as[JsObject]): _*)
-            case "$match"   => Match(jobj.\("$match").as[JsObject])
-            case "$project" => Project(jobj.\("$project").as[JsObject])
-            case "$group"   => getGroup(jobj.\("$group").as[JsObject])
-        }
-        return result
-
     }
 
-    def getGroup(jobj: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.PipelineOperator =
-        {
-            import collection.BatchCommands.AggregationFramework.{
-                AggregationResult,
-                Group,
-                GroupFunction
-            }
-            
-            val params = (jobj - "_id").keys.map {key =>
-                (key, getExpression((jobj \ key).as[JsObject]))
-            } toSeq
+    def getGroup(jobj: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.PipelineOperator = {
+        import collection.BatchCommands.AggregationFramework._
 
-            Group((jobj \ "_id").as[JsValue])(params: _*)
+        val params = (jobj - "_id").value.mapValues { value => getExpression(value.as[JsObject]) } toSeq
+
+        Group(jobj \ "_id")(params: _*)
+    }
+
+    def getExpression(expression: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.GroupFunction = {
+        import collection.BatchCommands.AggregationFramework._
+
+        expression.value.head match {
+            case ("$sum", JsNumber(n))      => SumValue(n.intValue)
+            case ("$sum", JsString(s))      => SumField(s)
+            case ("$avg", JsString(s))      => Avg(s)
+            case ("$min", JsString(s))      => Min(s)
+            case ("$max", JsString(s))      => Max(s)
+            case ("$push", JsString(s))     => Push(s)
+            case ("$addToSet", JsString(s)) => AddToSet(s)
+            case ("$first", JsString(s))    => First(s)
+            case ("$last", JsString(s))     => Last(s)
         }
+    }
 
+    def getSortOrder(orders: JsObject)(implicit collection: JSONCollection): Seq[collection.BatchCommands.AggregationFramework.SortOrder] = {
+        import collection.BatchCommands.AggregationFramework._
 
-    def getExpression(expression: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.GroupFunction =
-        {
-            import collection.BatchCommands.AggregationFramework.{
-                GroupFunction,
-                Last,
-                First,
-                AddToSet,
-                Push,
-                Max,
-                Min,
-                Avg
+        orders.value.map {
+            case (key, JsNumber(n)) => n.intValue match {
+                case 1  => Ascending(key)
+                case -1 => Descending(key)
             }
-
-            expression.keys.head match {
-                case "$sum"      => getSum(expression)
-                case "$avg"      => Avg((expression \ ("$avg")).as[String])
-                case "$min"      => Min((expression \ ("$min")).as[String])
-                case "$max"      => Max((expression \ ("$max")).as[String])
-                case "$push"     => Push((expression \ ("$push")).as[String])
-                case "$addToSet" => AddToSet((expression \ "$addToSet").as[String])
-                case "$first"    => First((expression \ ("$first")).as[String])
-                case "$last"     => Last((expression \ ("$last")).as[String])
-            }
-        }
-
-    def getSum(sum: JsObject)(implicit collection: JSONCollection): collection.BatchCommands.AggregationFramework.GroupFunction =
-        {
-            import collection.BatchCommands.AggregationFramework.{
-                GroupFunction,
-                SumField,
-                SumValue
-            }
-            val result: GroupFunction = sum.\("$sum") match {
-                case t: JsString => SumField(t.as[String])
-                case t: JsNumber => SumValue(t.as[Int])
-            }
-            return result
-        }
-
-    def getSortOrder(orders: JsObject)(implicit collection: JSONCollection) =
-        {
-            import collection.BatchCommands.AggregationFramework.{
-                Sort,
-                SortOrder,
-                Ascending,
-                Descending
-            }
-            var result: Array[SortOrder] = Array()
-
-            for (key <- orders.keys) {
-                val param = orders.\(key).as[Int] match {
-                    case 1  => result = result :+ Ascending(key)
-                    case -1 => result = result :+ Descending(key)
-                    case _  => // TODO unsupported and therefore ignored
-                }
-            }
-
-            result
-        }
-
+        } toSeq
+    }
 }
